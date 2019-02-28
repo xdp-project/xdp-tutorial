7df8ef972add98afd06aef0f07ca38b749238e3b
Jesper Dangaard Brouer <brouer@redhat.com>
2019-02-28 16:45:17 +0100

    Copy common_user.{c,h} from basic01-xdp-pass to basic02-prog-by-name
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ basic02-prog-by-name/common_user.h
@@ -0,0 +1,27 @@
+/* This common_user.h is used by userspace programs */
+#ifndef __COMMON_USER_H
+#define __COMMON_USER_H
+
+struct config {
+	__u32 xdp_flags;
+	int ifindex;
+	char *ifname;
+	char ifname_buf[IF_NAMESIZE];
+	bool do_unload;
+};
+
+/* Exit return codes */
+#define EXIT_OK		0 /* == EXIT_SUCCESS (stdlib.h) man exit(3) */
+#define EXIT_FAIL		1 /* == EXIT_FAILURE (stdlib.h) man exit(3) */
+#define EXIT_FAIL_OPTION	2
+#define EXIT_FAIL_XDP		30
+#define EXIT_FAIL_BPF		40
+
+void usage(const char *prog_name, const char *doc,
+           const struct option *long_options);
+
+void parse_cmdline_args(int argc, char **argv,
+			const struct option *long_options,
+                        struct config *cfg, const char *doc);
+
+#endif /* __COMMON_USER_H */
