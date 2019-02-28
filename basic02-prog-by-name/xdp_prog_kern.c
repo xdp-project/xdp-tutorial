44d50893745cafd9a01fb9467f912e60a632f236
Jesper Dangaard Brouer <brouer@redhat.com>
2019-02-28 22:30:39 +0100

    basic02-prog-by-name: remove XDP_ABORTED program
    As this will be an assignment to implement.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- basic02-prog-by-name/xdp_prog_kern.c
+++ basic02-prog-by-name/xdp_prog_kern.c
@@ -26,10 +26,15 @@ int  xdp_drop_func(struct xdp_md *ctx)
 	return XDP_DROP;
 }
 
-SEC("xdp_aborted")
-int  xdp_aborted_func(struct xdp_md *ctx)
-{
-	return XDP_ABORTED;
-}
-
 char _license[] SEC("license") = "GPL";
+
+/* Hint the avail XDP action return codes are:
+
+enum xdp_action {
+        XDP_ABORTED = 0,
+        XDP_DROP,
+        XDP_PASS,
+        XDP_TX,
+        XDP_REDIRECT,
+};
+*/
