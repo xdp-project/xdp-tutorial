549d7c48336c65d5ba5e5d176bdabb10dc9a6d96
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-04 23:08:57 +0100

    basic03-map-counter: implement stats counter in xdp_stats.c
    As we are considering asking tutorial partisipant to
    code per CPU and per RXQ stats, make the data structures
    more elaborate and easier to extend later.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- basic03-map-counter/xdp_stats.c
+++ basic03-map-counter/xdp_stats.c
@@ -8,6 +8,10 @@ static const char *__doc__ = "XDP loader and stats program\n"
 #include <errno.h>
 #include <getopt.h>
 
+#include <locale.h>
+#include <unistd.h>
+#include <time.h>
+
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
 
@@ -133,7 +137,7 @@ static void print_map_fd_info(int map_fd)
 		exit(EXIT_FAIL_BPF) ;
 	}
 	if (verbose)
-		printf("Info: BPF map (bpf_map_type:%d) id:%d name:%s\n",
+		printf(" - BPF map (bpf_map_type:%d) id:%d name:%s\n",
 		       info.type, info.id, info.name);
 }
 
@@ -202,10 +206,119 @@ int find_map_fd(struct bpf_object *bpf_obj, const char *mapname)
 	return map_fd;
 }
 
+#define NANOSEC_PER_SEC 1000000000 /* 10^9 */
+static __u64 gettime(void)
+{
+	struct timespec t;
+	int res;
+
+	res = clock_gettime(CLOCK_MONOTONIC, &t);
+	if (res < 0) {
+		fprintf(stderr, "Error with gettimeofday! (%i)\n", res);
+		exit(EXIT_FAIL);
+	}
+	return (__u64) t.tv_sec * NANOSEC_PER_SEC + t.tv_nsec;
+}
+
+/* TODO: Move this into common_kern_user.h */
+struct datarec {
+        __u64 rx_packets;
+};
+
+struct record {
+	__u64 timestamp;
+	struct datarec total;
+};
+
+struct stats_record {
+	struct record stats;
+};
+
+static double calc_period(struct record *r, struct record *p)
+{
+	double period_ = 0;
+	__u64 period = 0;
+
+	period = r->timestamp - p->timestamp;
+	if (period > 0)
+		period_ = ((double) period / NANOSEC_PER_SEC);
+
+	return period_;
+}
+
+static void stats_print(struct stats_record *stats_rec,
+			struct stats_record *stats_prev)
+{
+	struct record *rec, *prev;
+	double period;
+	__u64 packets;
+	double pps;
+
+	rec  = &stats_rec->stats;
+	prev = &stats_prev->stats;
+
+	period = calc_period(rec, prev);
+	if (period == 0)
+		return;
+
+	packets = rec->total.rx_packets - prev->total.rx_packets;
+	pps     = packets / period;
+
+	printf("XDP stats: %-7s RX-pkts:%'-10lld pps:%'-11.0f period:%f\n",
+	       "total", packets, pps, period);
+}
+
+static bool map_collect(int fd, __u32 key, struct record *rec)
+{
+	struct datarec value;
+
+	if ((bpf_map_lookup_elem(fd, &key, &value)) != 0) {
+		fprintf(stderr,
+			"ERR: bpf_map_lookup_elem failed key:0x%X\n", key);
+		return false;
+	}
+	/* Get time as close as possible to reading map contents */
+	rec->timestamp = gettime();
+
+	rec->total.rx_packets = value.rx_packets;
+	return true;
+}
+
+static void stats_collect(int map_fd, struct stats_record *stats_rec)
+{
+	map_collect(map_fd, 0, &stats_rec->stats);
+}
+
+static void stats_poll(int map_fd, int interval)
+{
+	struct stats_record prev, record = { 0 };
+
+	/* Trick to pretty printf with thousands separators use %' */
+	setlocale(LC_NUMERIC, "en_US");
+
+	/* Print stats "header" */
+	if (verbose) {
+		printf("\nCollecting stats from BPF map\n");
+		print_map_fd_info(map_fd);
+	}
+
+	/* Get initial reading quickly */
+	stats_collect(map_fd, &record);
+	usleep(1000000/4);
+
+	while (1) {
+		prev = record; /* struct copy */
+		stats_collect(map_fd, &record);
+		stats_print(&record, &prev);
+		sleep(interval);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	struct bpf_object *bpf_obj;
 	int stats_map_fd;
+	int interval = 2;
 
 	struct config cfg = {
 		.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
@@ -235,7 +348,7 @@ int main(int argc, char **argv)
 	if (stats_map_fd < 0)
 		return EXIT_FAIL_BPF;
 
-	print_map_fd_info(stats_map_fd);
+	stats_poll(stats_map_fd, interval);
 
 	return EXIT_OK;
 }
