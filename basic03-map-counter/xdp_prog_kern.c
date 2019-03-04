9dff5aa1a3cc6b59eef1d01549e57465f4ba7e7b
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-04 17:58:15 +0100

    basic03-map-counter: init with files from basic02-prog-by-name
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ basic03-map-counter/xdp_prog_kern.c
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include "bpf_helpers.h"
+
+/* Notice how this XDP/BPF-program contains several programs in the same source
+ * file. These will each get their own section in the ELF file, and via libbpf
+ * they can be selected individually, and via their file-descriptor attached to
+ * a given kernel BPF-hook.
+ *
+ * The libbpf bpf_object__find_program_by_title() refers to SEC names below.
+ * The iproute2 utility also use section name.
+ *
+ * Slightly confusing, the names that gets listed by "bpftool prog" are the
+ * C-function names (below the SEC define).
+ */
+
+SEC("xdp_pass")
+int  xdp_pass_func(struct xdp_md *ctx)
+{
+	return XDP_PASS;
+}
+
+SEC("xdp_drop")
+int  xdp_drop_func(struct xdp_md *ctx)
+{
+	return XDP_DROP;
+}
+
+char _license[] SEC("license") = "GPL";
+
+/* Hint the avail XDP action return codes are:
+
+enum xdp_action {
+        XDP_ABORTED = 0,
+        XDP_DROP,
+        XDP_PASS,
+        XDP_TX,
+        XDP_REDIRECT,
+};
+*/
