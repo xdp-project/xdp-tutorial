de55e7a9fed9d7b61696a10d79cc714e01bfb9b4
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-05 18:05:52 +0100

    basic03-map-counter/xdp_prog_kern.c change map name
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- basic03-map-counter/xdp_prog_kern.c
+++ basic03-map-counter/xdp_prog_kern.c
@@ -4,7 +4,7 @@
 
 #include "common_kern_user.h" /* defines: struct datarec; */
 
-struct bpf_map_def SEC("maps") stats_map = {
+struct bpf_map_def SEC("maps") stats_array_map = {
 	.type        = BPF_MAP_TYPE_ARRAY,
 	.key_size    = sizeof(__u32),
 	.value_size  = sizeof(struct datarec),
@@ -25,7 +25,7 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	__u32 key = 0;
 
 	/* Lookup in kernel BPF-side return pointer to actual data record */
-	rec = bpf_map_lookup_elem(&stats_map, &key);
+	rec = bpf_map_lookup_elem(&stats_array_map, &key);
 	/* BPF kernel-side verifier will reject program if the NULL pointer
 	 * check isn't performed here. Even-though this is a static array where
 	 * we know key lookup 0 always will succeed.
