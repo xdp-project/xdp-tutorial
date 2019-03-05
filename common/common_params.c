cb9b1e877ea058574279de3752a8fcbabe21b4a9
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-05 12:44:18 +0100

    Fix compile warning on long_options flag
    common_params.c:27:35: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘int * const’ [-Wformat=]
        printf(" flag (internal value:%d)",
    
    As we don't .flag then lets just simplify code and not access it.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- common/common_params.c
+++ common/common_params.c
@@ -23,14 +23,8 @@ void usage(const char *prog_name, const char *doc,
 	printf(" Listing options:\n");
 	for (i = 0; long_options[i].name != 0; i++) {
 		printf(" --%-12s", long_options[i].name);
-		if (long_options[i].flag != NULL)
-			printf(" flag (internal value:%d)",
-			       long_options[i].flag);
-		else {
-			if (long_options[i].val > 64) /* ord('A') = 65 */
-				printf(" short-option: -%c",
-				       long_options[i].val);
-		}
+		if (long_options[i].val > 64) /* ord('A') = 65 */
+			printf(" short-option: -%c", long_options[i].val);
 		printf("\n");
 	}
 	printf("\n");
