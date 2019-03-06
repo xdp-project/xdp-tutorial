89231147c244332a89e2817d33fe127091162a3b
Toke Høiland-Jørgensen <toke@redhat.com>
2019-03-06 15:25:27 +0100

    testenv: Use SPDX license tag instead of full GPL text
    Also, move to GPL-2.0+ instead of GPL-3
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- testenv/testenv.sh
+++ testenv/testenv.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
 # Script to setup and manage test environment for the XDP tutorial.
 # See README.org for instructions on how to use.
 #
 # Author:   Toke Høiland-Jørgensen (toke@redhat.com)
 # Date:     6 March 2019
 # Copyright (c) 2019 Red Hat
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 set -o errexit
 set -o nounset
