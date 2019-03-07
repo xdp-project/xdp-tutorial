602a358f7d1b36f0c2022a6c93444a3177961a34
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-07 16:26:15 +0100

    basic04: remove loader parts of xdp_stats.c
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- basic04-pinning-maps/xdp_stats.c
+++ basic04-pinning-maps/xdp_stats.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-static const char *__doc__ = "XDP loader and stats program\n"
-	" - Allows selecting BPF section --progsec name to XDP-attach to --dev\n";
+static const char *__doc__ = "XDP stats program\n"
+	" - Finding xdp_stats_map via --dev name info\n";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,19 +22,10 @@ static const char *__doc__ = "XDP loader and stats program\n"
 #include "../common/common_user_bpf_xdp.h"
 #include "common_kern_user.h"
 
-static const char *default_filename = "xdp_prog_kern.o";
-static const char *default_progsec = "xdp_stats1";
-
 static const struct option long_options[] = {
 	{"help",        no_argument,		NULL, 'h' },
 	{"dev",         required_argument,	NULL, 'd' },
-	{"skb-mode",    no_argument,		NULL, 'S' },
-	{"native-mode", no_argument,		NULL, 'N' },
-	{"auto-mode",   no_argument,		NULL, 'A' },
-	{"force",       no_argument,		NULL, 'F' },
-	{"unload",      no_argument,		NULL, 'U' },
 	{"quiet",       no_argument,		NULL, 'q' },
-	{"progsec",    required_argument,	NULL,  2  },
 	{0, 0, NULL,  0 }
 };
 
@@ -213,13 +204,10 @@ int main(int argc, char **argv)
 	int interval = 2;
 
 	struct config cfg = {
-		.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
 		.ifindex   = -1,
 		.do_unload = false,
 	};
-	/* Set default BPF-ELF object file and BPF program name */
-	strncpy(cfg.filename, default_filename, sizeof(cfg.filename));
-	strncpy(cfg.progsec,  default_progsec,  sizeof(cfg.progsec));
+
 	/* Cmdline options can change progsec */
 	parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
 
@@ -228,19 +216,6 @@ int main(int argc, char **argv)
 		fprintf(stderr, "ERR: required option --dev missing\n");
 		usage(argv[0], __doc__, long_options);
 		return EXIT_FAIL_OPTION;
-	}
-	if (cfg.do_unload)
-		return xdp_link_detach(cfg.ifindex, cfg.xdp_flags, 0);
-
-	bpf_obj = load_bpf_and_xdp_attach(&cfg);
-	if (!bpf_obj)
-		return EXIT_FAIL_BPF;
-
-	if (verbose) {
-		printf("Success: Loaded BPF-object(%s) and used section(%s)\n",
-		       cfg.filename, cfg.progsec);
-		printf(" - XDP prog attached on device:%s(ifindex:%d)\n",
-		       cfg.ifname, cfg.ifindex);
 	}
 
 	/* Lesson: Locate map file descriptor */
