db83167f3616579542a2272632ba751d81e3f9db
Toke Høiland-Jørgensen <toke@redhat.com>
2019-03-08 19:00:41 +0100

    testenv: Remove env directory in /sys/fs/bpf on teardown if it exists
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- testenv/testenv.sh
+++ testenv/testenv.sh
@@ -230,6 +230,7 @@ teardown()
     ip link del dev "$NS"
     ip netns del "$NS"
     rm -f "$STATEFILE"
+    [ -d "/sys/fs/bpf/$NS" ] && rmdir "/sys/fs/bpf/$NS" || true
 
     if [ -f "$STATEDIR/current" ]; then
         local CUR=$(< "$STATEDIR/current" )
