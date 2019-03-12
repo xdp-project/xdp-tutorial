xdp-project/xdp-tutorial

commit 71ca34a1cfd92beee7c7f321b020fa83225f4512

Consolidate common parts of Makefiles into common.mk
There's a lot of repetition in the Makefiles which is not really
needed (and leads to small deviations over timer). Move all the common
stuff into common.mk and just include that.

Make a few adjustments along the way to make sure things keep working.

Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

common/common.mk
@@ -1,0 +1,79 @@
+
+LLC ?= llc
+CLANG ?= clang
+CC ?= gcc
+
+XDP_C = ${XDP_TARGETS:=.c}
+XDP_OBJ = ${XDP_C:.c=.o}
+USER_C ?= ${USER_TARGETS:=.c}
+USER_OBJ := ${USER_C:.c=.o}
+
+OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
+
+COMMON_OBJS ?= $(COMMON_DIR)/common_params.o $(COMMON_DIR)/common_user_bpf_xdp.o
+
+# Create expansions for dependencies
+COMMON_H := ${COMMON_OBJS:.o=.h}
+
+CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
+CFLAGS += -I../headers/
+LDFLAGS ?= -L$(LIBBPF_DIR)
+
+LIBS = -lbpf -lelf
+
+all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
+
+.PHONY: clean $(CLANG) $(LLC)
+
+clean:
+	$(MAKE) -C $(LIBBPF_DIR) clean
+	$(MAKE) -C $(COMMON_DIR) clean
+	rm -f $(USER_TARGETS)
+	rm -f $(XDP_OBJ)
+	rm -f $(USER_OBJ)
+	rm -f *.ll
+	rm -f *~
+
+llvm-check: $(CLANG) $(LLC)
+	@for TOOL in $^ ; do \
+		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
+			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
+			exit 1; \
+		else true; fi; \
+	done
+
+$(OBJECT_LIBBPF):
+	@if [ ! -d $(LIBBPF_DIR) ]; then \
+		echo "Error: Need libbpf submodule"; \
+		echo "May need to run git submodule update --init"; \
+		exit 1; \
+	else \
+		cd $(LIBBPF_DIR) && $(MAKE) all; \
+		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
+	fi
+
+# Create dependency: detect if C-file change and touch H-file, to trigger
+# target $(COMMON_OBJS)
+$(COMMON_H): %.h: %.c
+	touch $@
+
+# Detect if any of common obj changed and create dependency on .h-files
+$(COMMON_OBJS): %.o: %.h
+	make -C $(COMMON_DIR)
+
+$(USER_TARGETS): %: $(USER_C) $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS)
+	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
+	 $< $(LIBS)
+
+$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_H)
+	$(CLANG) -S \
+	    -target bpf \
+	    -D __BPF_TRACING__ \
+	    $(CFLAGS) \
+	    -Wall \
+	    -Wno-unused-value \
+	    -Wno-pointer-sign \
+	    -Wno-compare-distinct-pointer-types \
+	    -Werror \
+	    -O2 -emit-llvm -c -g $<
+	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}

basic01-xdp-pass/Makefile
@@ -1,85 +1,16 @@
 # SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
 
-TARGETS := xdp_pass
+XDP_TARGETS := xdp_pass_kern
+USER_TARGETS := xdp_pass
+USER_C := xdp_pass_user.c
+
 
 LLC ?= llc
 CLANG ?= clang
 CC := gcc
 
 LIBBPF_DIR = ../libbpf/src/
+COMMON_DIR = ../common/
+COMMON_OBJS := $(COMMON_DIR)/common_params.o
 
-XDP_C = ${TARGETS:=_kern.c}
-XDP_OBJ = ${XDP_C:.c=.o}
-USER_C = ${TARGETS:=_user.c}
-USER_OBJ = ${USER_C:.c=.o}
-OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
-
-COMMON_OBJS := ../common/common_params.o
-# Supports adding more COMMON_OBJS
-# COMMON_OBJS += ../common/common_bpf.o
-
-# Create expansions for dependencies
-COMMON_H := ${COMMON_OBJS:.o=.h}
-
-CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
-CFLAGS += -I../headers/
-LDFLAGS ?= -L$(LIBBPF_DIR)
-
-LIBS = -lbpf -lelf
-
-all: llvm-check $(TARGETS) $(XDP_OBJ)
-
-.PHONY: clean $(CLANG) $(LLC)
-
-clean:
-	cd $(LIBBPF_DIR) && $(MAKE) clean;
-	make -C ../common/ clean
-	rm -f $(TARGETS)
-	rm -f $(XDP_OBJ)
-	rm -f $(USER_OBJ)
-	rm -f *.ll
-	rm -f *~
-
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-$(OBJECT_LIBBPF):
-	@if [ ! -d $(LIBBPF_DIR) ]; then \
-		echo "Error: Need libbpf submodule"; \
-		echo "May need to run git submodule update --init"; \
-		exit 1; \
-	else \
-		cd $(LIBBPF_DIR) && $(MAKE) all; \
-		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
-	fi
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
-# Detect if any of common obj changed and create dependency on .h-files
-$(COMMON_OBJS): %.o: %.h
-	make -C ../common/
-
-$(TARGETS): %: %_user.c $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS)
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
-	 $< $(LIBS)
-
-$(XDP_OBJ): %.o: %.c  Makefile
-	$(CLANG) -S \
-	    -target bpf \
-	    -D __BPF_TRACING__ \
-	    $(CFLAGS) \
-	    -Wall \
-	    -Wno-unused-value \
-	    -Wno-pointer-sign \
-	    -Wno-compare-distinct-pointer-types \
-	    -Werror \
-	    -O2 -emit-llvm -c -g $<
-	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}
+include $(COMMON_DIR)/common.mk

basic02-prog-by-name/Makefile
@@ -1,86 +1,10 @@
 # SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
 
-# Departing from the implicit _user.c scheme
 XDP_TARGETS  := xdp_prog_kern
 USER_TARGETS := xdp_loader
 
-LLC ?= llc
-CLANG ?= clang
-CC := gcc
-
 LIBBPF_DIR = ../libbpf/src/
+COMMON_DIR = ../common/
 
-XDP_C = ${XDP_TARGETS:=.c}
-XDP_OBJ = ${XDP_C:.c=.o}
-USER_C = ${USER_TARGETS:=.c}
-USER_OBJ = ${USER_C:.c=.o}
+include $(COMMON_DIR)/common.mk
 
-OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
-
-COMMON_OBJS := ../common/common_params.o
-COMMON_OBJS += ../common/common_user_bpf_xdp.o
-# Create expansions for dependencies
-COMMON_H := ${COMMON_OBJS:.o=.h}
-
-CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
-CFLAGS += -I../headers/
-LDFLAGS ?= -L$(LIBBPF_DIR)
-
-LIBS = -lbpf -lelf
-
-all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
-
-.PHONY: clean $(CLANG) $(LLC)
-
-clean:
-	cd $(LIBBPF_DIR) && $(MAKE) clean;
-	make -C ../common/ clean
-	rm -f $(USER_TARGETS)
-	rm -f $(XDP_OBJ)
-	rm -f $(USER_OBJ)
-	rm -f *.ll
-	rm -f *~
-
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-$(OBJECT_LIBBPF):
-	@if [ ! -d $(LIBBPF_DIR) ]; then \
-		echo "Error: Need libbpf submodule"; \
-		echo "May need to run: git submodule update --init"; \
-		exit 1; \
-	else \
-		cd $(LIBBPF_DIR) && $(MAKE) all; \
-		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
-	fi
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
-# Detect if any of common obj changed and create dependency on .h-files
-$(COMMON_OBJS): %.o: %.h
-	make -C ../common/
-
-$(USER_TARGETS): %: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS)
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
-	 $< $(LIBS)
-
-$(XDP_OBJ): %.o: %.c  Makefile
-	$(CLANG) -S \
-	    -target bpf \
-	    -D __BPF_TRACING__ \
-	    $(CFLAGS) \
-	    -Wall \
-	    -Wno-unused-value \
-	    -Wno-pointer-sign \
-	    -Wno-compare-distinct-pointer-types \
-	    -Werror \
-	    -O2 -emit-llvm -c -g $<
-	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}

basic03-map-counter/Makefile
@@ -4,83 +4,8 @@
 XDP_TARGETS  := xdp_prog_kern
 USER_TARGETS := xdp_load_and_stats
 
-LLC ?= llc
-CLANG ?= clang
-CC := gcc
-
 LIBBPF_DIR = ../libbpf/src/
+COMMON_DIR = ../common/
 
-XDP_C = ${XDP_TARGETS:=.c}
-XDP_OBJ = ${XDP_C:.c=.o}
-USER_C = ${USER_TARGETS:=.c}
-USER_OBJ = ${USER_C:.c=.o}
+include $(COMMON_DIR)/common.mk
 
-OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
-
-COMMON_OBJS := ../common/common_params.o
-COMMON_OBJS += ../common/common_user_bpf_xdp.o
-# Create expansions for dependencies
-COMMON_H := ${COMMON_OBJS:.o=.h}
-
-CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
-CFLAGS += -I../headers/
-LDFLAGS ?= -L$(LIBBPF_DIR)
-
-LIBS = -lbpf -lelf
-
-all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
-
-.PHONY: clean $(CLANG) $(LLC)
-
-clean:
-	cd $(LIBBPF_DIR) && $(MAKE) clean;
-	make -C ../common/ clean
-	rm -f $(USER_TARGETS)
-	rm -f $(XDP_OBJ)
-	rm -f $(USER_OBJ)
-	rm -f *.ll
-	rm -f *~
-
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-$(OBJECT_LIBBPF):
-	@if [ ! -d $(LIBBPF_DIR) ]; then \
-		echo "Error: Need libbpf submodule"; \
-		echo "May need to run: git submodule update --init"; \
-		exit 1; \
-	else \
-		cd $(LIBBPF_DIR) && $(MAKE) all; \
-		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
-	fi
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
-# Detect if any of common obj changed and create dependency on .h-files
-$(COMMON_OBJS): %.o: %.h
-	make -C ../common/
-
-$(USER_TARGETS): %: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS) common_kern_user.h
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
-	 $< $(LIBS)
-
-$(XDP_OBJ): %.o: %.c  common_kern_user.h Makefile
-	$(CLANG) -S \
-	    -target bpf \
-	    -D __BPF_TRACING__ \
-	    $(CFLAGS) \
-	    -Wall \
-	    -Wno-unused-value \
-	    -Wno-pointer-sign \
-	    -Wno-compare-distinct-pointer-types \
-	    -Werror \
-	    -O2 -emit-llvm -c -g $<
-	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}

basic04-pinning-maps/Makefile
@@ -2,86 +2,9 @@
 
 # Departing from the implicit _user.c scheme
 XDP_TARGETS  := xdp_prog_kern
-USER_TARGETS := xdp_loader
-USER_TARGETS += xdp_stats
-
-LLC ?= llc
-CLANG ?= clang
-CC := gcc
+USER_TARGETS := xdp_loader xdp_stats
 
 LIBBPF_DIR = ../libbpf/src/
+COMMON_DIR = ../common/
 
-XDP_C = ${XDP_TARGETS:=.c}
-XDP_OBJ = ${XDP_C:.c=.o}
-USER_C = ${USER_TARGETS:=.c}
-USER_OBJ = ${USER_C:.c=.o}
-
-OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
-
-COMMON_OBJS := ../common/common_params.o
-COMMON_OBJS += ../common/common_user_bpf_xdp.o
-# Create expansions for dependencies
-COMMON_H := ${COMMON_OBJS:.o=.h}
-
-CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
-CFLAGS += -I../headers/
-LDFLAGS ?= -L$(LIBBPF_DIR)
-
-LIBS = -lbpf -lelf
-
-all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
-
-.PHONY: clean $(CLANG) $(LLC)
-
-clean:
-	cd $(LIBBPF_DIR) && $(MAKE) clean;
-	make -C ../common/ clean
-	rm -f $(USER_TARGETS)
-	rm -f $(XDP_OBJ)
-	rm -f $(USER_OBJ)
-	rm -f *.ll
-	rm -f *~
-
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-$(OBJECT_LIBBPF):
-	@if [ ! -d $(LIBBPF_DIR) ]; then \
-		echo "Error: Need libbpf submodule"; \
-		echo "May need to run: git submodule update --init"; \
-		exit 1; \
-	else \
-		cd $(LIBBPF_DIR) && $(MAKE) all; \
-		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
-	fi
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
-# Detect if any of common obj changed and create dependency on .h-files
-$(COMMON_OBJS): %.o: %.h
-	make -C ../common/
-
-$(USER_TARGETS): %: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS) common_kern_user.h
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
-	 $< $(LIBS)
-
-$(XDP_OBJ): %.o: %.c  common_kern_user.h Makefile
-	$(CLANG) -S \
-	    -target bpf \
-	    -D __BPF_TRACING__ \
-	    $(CFLAGS) \
-	    -Wall \
-	    -Wno-unused-value \
-	    -Wno-pointer-sign \
-	    -Wno-compare-distinct-pointer-types \
-	    -Werror \
-	    -O2 -emit-llvm -c -g $<
-	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}
+include $(COMMON_DIR)/common.mk

packet01-parsing/Makefile
@@ -1,86 +1,9 @@
 # SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
 
-# Departing from the implicit _user.c scheme
 XDP_TARGETS  := xdp_prog_kern
 USER_TARGETS :=
 
-LLC ?= llc
-CLANG ?= clang
-CC := gcc
-
 LIBBPF_DIR = ../libbpf/src/
+COMMON_DIR = ../common/
 
-XDP_C = ${XDP_TARGETS:=.c}
-XDP_OBJ = ${XDP_C:.c=.o}
-USER_C = ${USER_TARGETS:=.c}
-USER_OBJ = ${USER_C:.c=.o}
-
-OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
-
-COMMON_OBJS := ../common/common_params.o
-COMMON_OBJS += ../common/common_user_bpf_xdp.o
-# Create expansions for dependencies
-COMMON_H := ${COMMON_OBJS:.o=.h}
-
-CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
-CFLAGS += -I../headers/
-LDFLAGS ?= -L$(LIBBPF_DIR)
-
-LIBS = -lbpf -lelf
-
-all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
-
-.PHONY: clean $(CLANG) $(LLC)
-
-clean:
-	cd $(LIBBPF_DIR) && $(MAKE) clean;
-	make -C ../common/ clean
-	rm -f $(USER_TARGETS)
-	rm -f $(XDP_OBJ)
-	rm -f $(USER_OBJ)
-	rm -f *.ll
-	rm -f *~
-
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-$(OBJECT_LIBBPF):
-	@if [ ! -d $(LIBBPF_DIR) ]; then \
-		echo "Error: Need libbpf submodule"; \
-		echo "May need to run: git submodule update --init"; \
-		exit 1; \
-	else \
-		cd $(LIBBPF_DIR) && $(MAKE) all; \
-		mkdir -p root; DESTDIR=root $(MAKE) install_headers; \
-	fi
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
-# Detect if any of common obj changed and create dependency on .h-files
-$(COMMON_OBJS): %.o: %.h
-	make -C ../common/
-
-$(USER_TARGETS): %: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS) common_kern_user.h
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
-	 $< $(LIBS)
-
-$(XDP_OBJ): %.o: %.c  Makefile
-	$(CLANG) -S \
-	    -target bpf \
-	    -D __BPF_TRACING__ \
-	    $(CFLAGS) \
-	    -Wall \
-	    -Wno-unused-value \
-	    -Wno-pointer-sign \
-	    -Wno-compare-distinct-pointer-types \
-	    -Werror \
-	    -O2 -emit-llvm -c -g $<
-	$(LLC) -march=bpf -filetype=obj -o $@ ${@:.o=.ll}
+include $(COMMON_DIR)/common.mk

commit 516e48be10dbd9783bb5a196d0e31b85ea998e0e

Fix: dependency on common_kern_user.h were also lost
Duing the conversion to common.mk

Fixes: 71ca34a1cfd9 ("Consolidate common parts of Makefiles into common.mk")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -18,6 +18,9 @@ COMMON_OBJS ?= $(COMMON_DIR)/common_params.o $(COMMON_DIR)/common_user_bpf_xdp.o
 # Create expansions for dependencies
 COMMON_H := ${COMMON_OBJS:.o=.h}
 
+# BPF-prog kern and userspace shares struct via header file:
+KERN_USER_H ?= $(wildcard "common_kern_user.h")
+
 CFLAGS ?= -I$(LIBBPF_DIR)/root/usr/include/
 CFLAGS += -I../headers/
 LDFLAGS ?= -L$(LIBBPF_DIR)
@@ -75,11 +78,11 @@ $(COMMON_H): %.h: %.c
 $(COMMON_OBJS): %.o: %.h
 	make -C $(COMMON_DIR)
 
-$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS)
+$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS) $(KERN_USER_H)
 	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
 	 $< $(LIBS)
 
-$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(COMMON_H)
+$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(KERN_USER_H)
 	$(CLANG) -S \
 	    -target bpf \
 	    -D __BPF_TRACING__ \