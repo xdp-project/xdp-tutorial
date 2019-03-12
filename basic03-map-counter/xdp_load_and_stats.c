bffaa6087138e9f3239364804e9c14e9167018d2
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-12 17:43:46 +0100

    basic03: improve Lesson#4: that userspace check map info
    It is userspace responsibility to known what map it is reading and
    know the value size. Here get bpf_map_info and check if it match our
    expected sizes.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- basic03-map-counter/xdp_load_and_stats.c
+++ basic03-map-counter/xdp_load_and_stats.c
@@ -60,29 +60,6 @@ static const struct option_wrapper long_options[] = {
 	{{0, 0, NULL,  0 }}
 };
 
-static __u32 get_map_fd_type(int map_fd)
-{
-	struct bpf_map_info info = {};
-	__u32 info_len = sizeof(info);
-	int err;
-
-	if (map_fd < 0)
-		return BPF_MAP_TYPE_UNSPEC;
-
-        /* BPF-info via bpf-syscall */
-	err = bpf_obj_get_info_by_fd(map_fd, &info, &info_len);
-	if (err) {
-		fprintf(stderr, "ERR: %s() can't get info - %s\n",
-			__func__,  strerror(errno));
-		exit(EXIT_FAIL_BPF) ;
-	}
-	if (verbose)
-		printf(" - BPF map (bpf_map_type:%d) id:%d name:%s\n",
-		       info.type, info.id, info.name);
-
-	return info.type;
-}
-
 int find_map_fd(struct bpf_object *bpf_obj, const char *mapname)
 {
 	struct bpf_map *map;
@@ -176,8 +153,8 @@ void map_get_value_array(int fd, __u32 key, struct datarec *value)
 void map_get_value_percpu_array(int fd, __u32 key, struct datarec *value)
 {
 	/* For percpu maps, userspace gets a value per possible CPU */
-	unsigned int nr_cpus = bpf_num_possible_cpus();
-	struct datarec values[nr_cpus];
+	// unsigned int nr_cpus = bpf_num_possible_cpus();
+	// struct datarec values[nr_cpus];
 
 	fprintf(stderr, "ERR: %s() not impl. see assignment#3", __func__);
 }
@@ -216,18 +193,12 @@ static void stats_collect(int map_fd, __u32 map_type,
 	map_collect(map_fd, map_type, key, &stats_rec->stats[0]);
 }
 
-static void stats_poll(int map_fd, int interval)
+static void stats_poll(int map_fd, __u32 map_type, int interval)
 {
 	struct stats_record prev, record = { 0 };
-	__u32 map_type;
 
 	/* Trick to pretty printf with thousands separators use %' */
 	setlocale(LC_NUMERIC, "en_US");
-
-	if (verbose) {
-		printf("\nCollecting stats from BPF map\n");
-	}
-	map_type = get_map_fd_type(map_fd);
 
 	/* Print stats "header" */
 	if (verbose) {
@@ -247,11 +218,63 @@ static void stats_poll(int map_fd, int interval)
 	}
 }
 
+/* Lesson#4: It is userspace responsibility to known what map it is reading and
+ * know the value size. Here get bpf_map_info and check if it match our expected
+ * values.
+ */
+static int check_map_fd_info(int map_fd, struct bpf_map_info *info,
+			     struct bpf_map_info *exp)
+{
+	__u32 info_len = sizeof(*info);
+	int err;
+
+	if (map_fd < 0)
+		return EXIT_FAIL;
+
+        /* BPF-info via bpf-syscall */
+	err = bpf_obj_get_info_by_fd(map_fd, info, &info_len);
+	if (err) {
+		fprintf(stderr, "ERR: %s() can't get info - %s\n",
+			__func__,  strerror(errno));
+		return EXIT_FAIL_BPF;
+	}
+
+	if (exp->key_size && exp->key_size != info->key_size) {
+		fprintf(stderr, "ERR: %s() "
+			"Map key size(%d) mismatch expected size(%d)\n",
+			__func__, info->key_size, exp->key_size);
+		return EXIT_FAIL;
+	}
+	if (exp->value_size && exp->value_size != info->value_size) {
+		fprintf(stderr, "ERR: %s() "
+			"Map value size(%d) mismatch expected size(%d)\n",
+			__func__, info->value_size, exp->value_size);
+		return EXIT_FAIL;
+	}
+	if (exp->max_entries && exp->max_entries != info->max_entries) {
+		fprintf(stderr, "ERR: %s() "
+			"Map max_entries(%d) mismatch expected size(%d)\n",
+			__func__, info->max_entries, exp->max_entries);
+		return EXIT_FAIL;
+	}
+	if (exp->type && exp->type  != info->type) {
+		fprintf(stderr, "ERR: %s() "
+			"Map type(%d) mismatch expected type(%d)\n",
+			__func__, info->type, exp->type);
+		return EXIT_FAIL;
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
+	struct bpf_map_info map_expect = { 0 };
+	struct bpf_map_info info = { 0 };
 	struct bpf_object *bpf_obj;
 	int stats_map_fd;
 	int interval = 2;
+	int err;
 
 	struct config cfg = {
 		.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
@@ -291,7 +314,24 @@ int main(int argc, char **argv)
 		return EXIT_FAIL_BPF;
 	}
 
-	stats_poll(stats_map_fd, interval);
+	/* Lesson#4: check map info, e.g. datarec is expected size */
+	map_expect.key_size    = sizeof(__u32);
+	map_expect.value_size  = sizeof(struct datarec);
+	map_expect.max_entries = XDP_ACTION_MAX;
+	err = check_map_fd_info(stats_map_fd, &info, &map_expect);
+	if (err) {
+		fprintf(stderr, "ERR: map via FD not compatible\n");
+		return err;
+	}
+	if (verbose) {
+		printf("\nCollecting stats from BPF map\n");
+		printf(" - BPF map (bpf_map_type:%d) id:%d name:%s"
+		       " key_size:%d value_size:%d max_entries:%d\n",
+		       info.type, info.id, info.name,
+		       info.key_size, info.value_size, info.max_entries
+		       );
+	}
 
+	stats_poll(stats_map_fd, info.type, interval);
 	return EXIT_OK;
 }
