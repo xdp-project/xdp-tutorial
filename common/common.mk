xdp-project/xdp-tutorial

commit 20dae1dcaae56a687866dd1bfb326c353160dd14

Add build dependency if common.mk change
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -41,6 +41,13 @@ $(COPY_LOADER): $(LOADER_DIR)/${COPY_LOADER:=.c} $(COMMON_H)
 	cp $(LOADER_DIR)/$(COPY_LOADER) $(COPY_LOADER)
 endif
 
+# For build dependency on this file, if it gets updated
+ifdef COMMON_DIR
+COMMON_MK = $(COMMON_DIR)/common.mk
+else
+COMMON_MK = ../common/common.mk
+endif
+
 llvm-check: $(CLANG) $(LLC)
 	@for TOOL in $^ ; do \
 		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
@@ -68,11 +75,11 @@ $(COMMON_H): %.h: %.c
 $(COMMON_OBJS): %.o: %.h
 	make -C $(COMMON_DIR)
 
-$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_OBJS)
+$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS)
 	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
 	 $< $(LIBS)
 
-$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_H)
+$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(COMMON_H)
 	$(CLANG) -S \
 	    -target bpf \
 	    -D __BPF_TRACING__ \

commit 5e8a73499f070a9a37cce6685234cfda9126e606

Improve on Makefile common.mk
As discussed with Toke other parts of common.mk depend on $(COMMON_DIR),
not only $(COMMON_MK), as done in commit 20dae1dcaae5 with Makefile ifdef.

It is much simpler to use =? and define COMMON_DIR if it was not
defined by including Makefile.  This also takes care of other
parts that depend on COMMON_DIR beind defined.

Fixes: 20dae1dcaae5 ("Add build dependency if common.mk change")
Suggested-by: Toke Høiland-Jørgensen <toke@redhat.com>
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -19,6 +19,9 @@ XDP_OBJ = ${XDP_C:.c=.o}
 USER_C := ${USER_TARGETS:=.c}
 USER_OBJ := ${USER_C:.c=.o}
 
+# Expect this is defined by including Makefile, but define if not
+COMMON_DIR ?= ../common/
+
 COPY_LOADER ?=
 LOADER_DIR := $(COMMON_DIR)/../basic04-pinning-maps
 
@@ -56,11 +59,7 @@ $(COPY_LOADER): $(LOADER_DIR)/${COPY_LOADER:=.c} $(COMMON_H)
 endif
 
 # For build dependency on this file, if it gets updated
-ifdef COMMON_DIR
 COMMON_MK = $(COMMON_DIR)/common.mk
-else
-COMMON_MK = ../common/common.mk
-endif
 
 llvm-check: $(CLANG) $(LLC)
 	@for TOOL in $^ ; do \