xdp-project/xdp-tutorial

commit e612c5a46fd6b56b74e5f60fefdc4952fa48144b

basic03+04: Assignment#3: Per CPU stats
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

basic04-pinning-maps/xdp_prog_kern.c
@@ -9,7 +9,7 @@
  * - The idea is to keep stats per (enum) xdp_action
  */
 struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_ARRAY,
+	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
 	.key_size    = sizeof(__u32),
 	.value_size  = sizeof(struct datarec),
 	.max_entries = XDP_ACTION_MAX,

basic03-map-counter/README.org
@@ -183,3 +183,42 @@ The *assignment* is primarily to extend userspace stats tool (in
 
 Avoid the atomic stats counter, by using another per-CPU array type, and
 move the burden of summing to userspace.
+
+First step is to change map =type= (in [[file:xdp_prog_kern.c]]) to use
+=BPF_MAP_TYPE_PERCPU_ARRAY=. If you only make this change, the userspace
+program will detect this and complain, as we query the map FD for some info
+(via =bpf_obj_get_info_by_fd()=) and e.g. check the map type. Remember it is
+userspace responsibility to make sure the data record for the value is large
+enough.
+
+Next step is writing a function that gets the values per CPU and sum these.
+In the [[file:xdp_load_and_stats.c]]. You can copy paste this, and call it from
+the switch-case statement in function =map_collect()=:
+
+#+begin_src C
+/* BPF_MAP_TYPE_PERCPU_ARRAY */
+void map_get_value_percpu_array(int fd, __u32 key, struct datarec *value)
+{
+	/* For percpu maps, userspace gets a value per possible CPU */
+	unsigned int nr_cpus = bpf_num_possible_cpus();
+	struct datarec values[nr_cpus];
+	__u64 sum_bytes = 0;
+	__u64 sum_pkts = 0;
+	int i;
+
+	if ((bpf_map_lookup_elem(fd, &key, values)) != 0) {
+		fprintf(stderr,
+			"ERR: bpf_map_lookup_elem failed key:0x%X\n", key);
+		return;
+	}
+
+	/* Sum values from each CPU */
+	for (i = 0; i < nr_cpus; i++) {
+		sum_pkts  += values[i].rx_packets;
+		sum_bytes += values[i].rx_bytes;
+	}
+	value->rx_packets = sum_pkts;
+	value->rx_bytes   = sum_bytes;
+}
+#+end_src
+

basic03-map-counter/xdp_load_and_stats.c
@@ -21,6 +21,7 @@ static const char *__doc__ = "XDP loader and stats program\n"
 #include "../common/common_params.h"
 #include "../common/common_user_bpf_xdp.h"
 #include "common_kern_user.h"
+#include "bpf_util.h" /* bpf_num_possible_cpus */
 
 static const char *default_filename = "xdp_prog_kern.o";
 static const char *default_progsec = "xdp_stats1";
@@ -162,12 +163,23 @@ static void stats_print(struct stats_record *stats_rec,
 	}
 }
 
+/* BPF_MAP_TYPE_ARRAY */
 void map_get_value_array(int fd, __u32 key, struct datarec *value)
 {
 	if ((bpf_map_lookup_elem(fd, &key, value)) != 0) {
 		fprintf(stderr,
 			"ERR: bpf_map_lookup_elem failed key:0x%X\n", key);
 	}
+}
+
+/* BPF_MAP_TYPE_PERCPU_ARRAY */
+void map_get_value_percpu_array(int fd, __u32 key, struct datarec *value)
+{
+	/* For percpu maps, userspace gets a value per possible CPU */
+	unsigned int nr_cpus = bpf_num_possible_cpus();
+	struct datarec values[nr_cpus];
+
+	fprintf(stderr, "ERR: %s() not impl. see assignment#3", __func__);
 }
 
 static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)

basic04-pinning-maps/xdp_stats.c
@@ -23,6 +23,7 @@ static const char *__doc__ = "XDP stats program\n"
 #include "../common/common_params.h"
 #include "../common/common_user_bpf_xdp.h"
 #include "common_kern_user.h"
+#include "bpf_util.h" /* bpf_num_possible_cpus */
 
 static const struct option_wrapper long_options[] = {
 	{{"help",        no_argument,		NULL, 'h' },
@@ -141,12 +142,39 @@ static void stats_print(struct stats_record *stats_rec,
 	printf("\n");
 }
 
+
+/* BPF_MAP_TYPE_ARRAY */
 void map_get_value_array(int fd, __u32 key, struct datarec *value)
 {
 	if ((bpf_map_lookup_elem(fd, &key, value)) != 0) {
 		fprintf(stderr,
 			"ERR: bpf_map_lookup_elem failed key:0x%X\n", key);
 	}
+}
+
+/* BPF_MAP_TYPE_PERCPU_ARRAY */
+void map_get_value_percpu_array(int fd, __u32 key, struct datarec *value)
+{
+	/* For percpu maps, userspace gets a value per possible CPU */
+	unsigned int nr_cpus = bpf_num_possible_cpus();
+	struct datarec values[nr_cpus];
+	__u64 sum_bytes = 0;
+	__u64 sum_pkts = 0;
+	int i;
+
+	if ((bpf_map_lookup_elem(fd, &key, values)) != 0) {
+		fprintf(stderr,
+			"ERR: bpf_map_lookup_elem failed key:0x%X\n", key);
+		return;
+	}
+
+	/* Sum values from each CPU */
+	for (i = 0; i < nr_cpus; i++) {
+		sum_pkts  += values[i].rx_packets;
+		sum_bytes += values[i].rx_bytes;
+	}
+	value->rx_packets = sum_pkts;
+	value->rx_bytes   = sum_bytes;
 }
 
 static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
@@ -161,7 +189,8 @@ static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
 		map_get_value_array(fd, key, &value);
 		break;
 	case BPF_MAP_TYPE_PERCPU_ARRAY:
-		/* fall-through */
+		map_get_value_percpu_array(fd, key, &value);
+		break;
 	default:
 		fprintf(stderr, "ERR: Unknown map_type(%u) cannot handle\n",
 			map_type);

commit 607a6e8263f3fdf14a21904fc78452b42d0b4993

basic04: finish assignment#3: Avoid the atomic operation
Forgot to complete the basic03 assignment#3.

Fixes: e612c5a46fd6 ("basic03+04: Assignment#3: Per CPU stats")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

basic04-pinning-maps/xdp_prog_kern.c
@@ -43,16 +43,12 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	/* Calculate packet length */
 	bytes = data_end - data;
 
-	/* Multiple CPUs can access data record. Thus, the accounting needs to
-	 * use an atomic operation.
+	/* BPF_MAP_TYPE_PERCPU_ARRAY returns a data record specific to current
+	 * CPU and XDP hooks runs under Softirq, which makes it safe to update
+	 * without atomic operations.
 	 */
-	lock_xadd(&rec->rx_packets, 1);
-	lock_xadd(&rec->rx_bytes, bytes);
-
-        /*
-         * Assignment#3: Avoid the atomic operation
-         * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
-         */
+	rec->rx_packets++;
+	rec->rx_bytes += bytes;
 
 	return XDP_PASS;
 }