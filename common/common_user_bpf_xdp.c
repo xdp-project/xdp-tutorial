09c45a6c7a92dc89a97e2c1b9103979913dfdbde
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-13 09:30:05 +0100

    HW-offload: enable offloading in basic02-prog-by-name/xdp_loader.c
    Also update common code:
    E.g change load_bpf_object_file() to apply the ifindex,
    and load_bpf_and_xdp_attach only use the ifindex iif
    offload flag XDP_FLAGS_HW_MODE is requested.
    
    I wonder if it will be okay to generally set the ifindex, even if
    not using offload flag XDP_FLAGS_HW_MODE.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- common/common_user_bpf_xdp.c
+++ common/common_user_bpf_xdp.c
@@ -7,6 +7,8 @@
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
 
+#include <linux/if_link.h> /* Need XDP flags */
+
 #include "common_defines.h"
 
 int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd)
@@ -77,17 +79,19 @@ int xdp_link_detach(int ifindex, __u32 xdp_flags, __u32 expected_prog_id)
 	return EXIT_OK;
 }
 
-struct bpf_object *load_bpf_object_file(const char *filename)
+struct bpf_object *load_bpf_object_file(const char *filename, int ifindex)
 {
 	int first_prog_fd = -1;
 	struct bpf_object *obj;
 	int err;
 
-	/* This struct allow us to set ifindex, but we currently don't use
-	 * features (like hardware offload) that requires setting ifindex.
+	/* This struct allow us to set ifindex, this features is used for
+	 * hardware offloading XDP programs (note this sets libbpf
+	 * bpf_program->prog_ifindex and foreach bpf_map->map_ifindex).
 	 */
 	struct bpf_prog_load_attr prog_load_attr = {
-		.prog_type      = BPF_PROG_TYPE_XDP,
+		.prog_type = BPF_PROG_TYPE_XDP,
+		.ifindex   = ifindex,
 	};
 	prog_load_attr.file = filename;
 
@@ -109,11 +113,16 @@ struct bpf_object *load_bpf_and_xdp_attach(struct config *cfg)
 {
 	struct bpf_program *bpf_prog;
 	struct bpf_object *bpf_obj;
+	int offload_ifindex = 0;
 	int prog_fd = -1;
 	int err;
 
+	/* If flags indicate hardware offload, supply ifindex */
+	if (cfg->xdp_flags & XDP_FLAGS_HW_MODE)
+		offload_ifindex = cfg->ifindex;
+
 	/* Load the BPF-ELF object file and get back libbpf bpf_object */
-	bpf_obj = load_bpf_object_file(cfg->filename);
+	bpf_obj = load_bpf_object_file(cfg->filename, offload_ifindex);
 	if (!bpf_obj) {
 		fprintf(stderr, "ERR: loading file: %s\n", cfg->filename);
 		exit(EXIT_FAIL_BPF);
