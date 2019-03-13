xdp-project/xdp-tutorial

commit efa735fff0d69b563e3f99015e2f601b92ac5a82

common.mk: Add EXTRA_DEPS to allow dirs to specify extra dependencies
Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

common/common.mk
@@ -32,6 +32,8 @@ COMMON_OBJS ?= $(COMMON_DIR)/common_params.o $(COMMON_DIR)/common_user_bpf_xdp.o
 # Create expansions for dependencies
 COMMON_H := ${COMMON_OBJS:.o=.h}
 
+EXTRA_DEPS :=
+
 # BPF-prog kern and userspace shares struct via header file:
 KERN_USER_H ?= $(wildcard "common_kern_user.h")
 
@@ -88,11 +90,11 @@ $(COMMON_H): %.h: %.c
 $(COMMON_OBJS): %.o: %.h
 	make -C $(COMMON_DIR)
 
-$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS) $(KERN_USER_H)
+$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS) $(KERN_USER_H) $(EXTRA_DEPS)
 	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
 	 $< $(LIBS)
 
-$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(KERN_USER_H)
+$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(KERN_USER_H) $(EXTRA_DEPS)
 	$(CLANG) -S \
 	    -target bpf \
 	    -D __BPF_TRACING__ \

commit 6460f470cc0f6562660f6c431ce6d80da01806fd

Fix: EXTRA_DEPS feature in common.mk
The EXTRA_DEPS in common.mk must not use ":=" as it will
overwrite the ones set in the including Makefile.

Change this to use "+=" instead.

Fixes: efa735fff0d6 ("common.mk: Add EXTRA_DEPS to allow dirs to specify extra dependencies")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -33,7 +33,7 @@ COMMON_OBJS += $(COMMON_DIR)/common_params.o $(COMMON_DIR)/common_user_bpf_xdp.o
 # Create expansions for dependencies
 COMMON_H := ${COMMON_OBJS:.o=.h}
 
-EXTRA_DEPS :=
+EXTRA_DEPS +=
 
 # BPF-prog kern and userspace shares struct via header file:
 KERN_USER_H ?= $(wildcard "common_kern_user.h")

packet02-rewriting/Makefile
@@ -9,6 +9,5 @@ COMMON_DIR = ../common
 COPY_LOADER := xdp_loader
 EXTRA_DEPS := $(COMMON_DIR)/parsing_helpers.h
 
-
 include $(COMMON_DIR)/common.mk
 