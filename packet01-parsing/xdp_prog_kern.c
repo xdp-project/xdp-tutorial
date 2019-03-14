86abb58331e5b2c1e9cb36037fcfab9c07891067
Toke Høiland-Jørgensen <toke@redhat.com>
2019-03-14 17:10:30 +0100

    packet01: Add assignments and a few more initial notes
    This also removes the "solution" code from packet01, which basically
    completes that lesson.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- packet01-parsing/xdp_prog_kern.c
+++ packet01-parsing/xdp_prog_kern.c
@@ -27,7 +27,7 @@ static __always_inline int parse_ethhdr(void **nexthdr, void *data_end,
 	/* Byte-count bounds check; check if current pointer + size of header
 	 * is after data_end.
 	 */
-	if (*nexthdr + hdrsize > data_end)
+	if (*nexthdr + 1 > data_end)
 		return -1;
 
 	*nexthdr += hdrsize;
@@ -36,39 +36,19 @@ static __always_inline int parse_ethhdr(void **nexthdr, void *data_end,
 	return bpf_ntohs(eth->h_proto);
 }
 
-static __always_inline int parse_ip6hdr(void **nexthdr,
+/* Assignment 2: Implement and use this */
+/*static __always_inline int parse_ip6hdr(void **nexthdr,
 					void *data_end,
 					struct ipv6hdr **ip6hdr)
 {
-	struct ipv6hdr *ip6h = *nexthdr;
+}*/
 
-	/* Pointer-arithmetic bounds check; pointer +1 points to after end of
-	 * thing being pointed to. We will be using this style in the remainder
-	 * of the tutorial.
-	 */
-	if (ip6h + 1 > data_end)
-		return -1;
-
-	*nexthdr = ip6h + 1;
-	*ip6hdr = ip6h;
-
-	return ip6h->nexthdr;
-}
-
-static __always_inline int parse_icmp6hdr(void **nexthdr,
+/* Assignment 3: Implement and use this */
+/*static __always_inline int parse_icmp6hdr(void **nexthdr,
 					  void *data_end,
 					  struct icmp6hdr **icmp6hdr)
 {
-	struct icmp6hdr *icmp6h = *nexthdr;
-
-	if (icmp6h + 1 > data_end)
-		return -1;
-
-	*nexthdr = icmp6h + 1;
-	*icmp6hdr = icmp6h;
-
-	return icmp6h->icmp6_type;
-}
+}*/
 
 SEC("xdp_packet_parser")
 int  xdp_parser_func(struct xdp_md *ctx)
@@ -81,8 +61,6 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	int nh_type;
 
 	struct ethhdr *eth;
-	struct ipv6hdr *ip6h;
-	struct icmp6hdr *icmp6h;
 
 	/* Packet parsing in steps: Get each header one at a time, aborting if
 	 * parsing fails. Each helper function does sanity checking (is the
@@ -92,19 +70,9 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	if (nh_type != ETH_P_IPV6)
 		goto out;
 
-	nh_type = parse_ip6hdr(&nh_ptr, data_end, &ip6h);
-	if (nh_type != IPPROTO_ICMPV6)
-		goto out;
+	/* Assignment additions go below here */
 
-	nh_type = parse_icmp6hdr(&nh_ptr, data_end, &icmp6h);
-	if (nh_type != ICMPV6_ECHO_REQUEST)
-		goto out;
-
-	/* Perform the actual function we wanted, namely to drop all
-	 * even-numbered ping packets.
-	 */
-	if (bpf_ntohs(icmp6h->icmp6_sequence) % 2 == 0)
-		return XDP_DROP;
+	return XDP_DROP;
 
 out:
 	/* Everything we couldn't parse, or that we don't want to deal with, we
