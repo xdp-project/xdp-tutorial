c8f01b9a5239e3d1532b54f2208f8243761c97c4
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-15 13:49:47 +0100

    packet01: use struct for next header pointer iterator
    This is basically C syntax-sugar for the samething that is
    currently implemented, there should be no functional change.
    
    This is an attempt to make the code easier to understand for
    people that are not C-experts. While the code is correct, I fear
    that people will be scared when seeing a void **ptr.
    
    When introducing a simple struct with a pointer, we can make it
    more clear that we are looking at the pointer value in the bound
    check
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- packet01-parsing/xdp_prog_kern.c
+++ packet01-parsing/xdp_prog_kern.c
@@ -9,6 +9,11 @@
 #include "bpf_helpers.h"
 #include "bpf_endian.h"
 
+/* Next header iterator for packet parsing */
+struct next_hdr_iter {
+	void *ptr;
+};
+
 /* Packet parsing helpers.
  *
  * Each helper parses a packet header, including doing bounds checking, and
@@ -18,19 +23,20 @@
  * (h_proto for Ethernet, nexthdr for IPv6), for ICMP it is the ICMP type field.
  * All return values are in host byte order.
  */
-static __always_inline int parse_ethhdr(void **nexthdr, void *data_end,
+static __always_inline int parse_ethhdr(struct next_hdr_iter *nh,
+					void *data_end,
 					struct ethhdr **ethhdr)
 {
-	struct ethhdr *eth = *nexthdr;
+	struct ethhdr *eth = nh->ptr;
 	int hdrsize = sizeof(*eth);
 
 	/* Byte-count bounds check; check if current pointer + size of header
 	 * is after data_end.
 	 */
-	if (*nexthdr + 1 > data_end)
+	if (nh->ptr + 1 > data_end)
 		return -1;
 
-	*nexthdr += hdrsize;
+	nh->ptr += hdrsize;
 	*ethhdr = eth;
 
 	return bpf_ntohs(eth->h_proto);
@@ -55,18 +61,20 @@ int  xdp_parser_func(struct xdp_md *ctx)
 {
 	void *data_end = (void *)(long)ctx->data_end;
 	void *data = (void *)(long)ctx->data;
+	struct ethhdr *eth;
 
-        /* These keep track of the next header type and a pointer to it */
-	void *nh_ptr = data;
+        /* These keep track of the next header type and iterator pointer */
+	struct next_hdr_iter nh;
 	int nh_type;
 
-	struct ethhdr *eth;
+	/* Start next header iterator pointer at data start */
+	nh->ptr = data;
 
 	/* Packet parsing in steps: Get each header one at a time, aborting if
 	 * parsing fails. Each helper function does sanity checking (is the
 	 * header type in the packet correct?), and bounds checking.
 	 */
-	nh_type = parse_ethhdr(&nh_ptr, data_end, &eth);
+	nh_type = parse_ethhdr(&nh, data_end, &eth);
 	if (nh_type != ETH_P_IPV6)
 		goto out;
 
