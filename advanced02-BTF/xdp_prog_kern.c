61e53814c41c5b1e138fea3f34668c5621d3c05c
Jesper Dangaard Brouer <brouer@redhat.com>
2019-03-17 16:08:29 +0100

    advanced02-BTF: copy BPF code from basic04
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ advanced02-BTF/xdp_prog_kern.c
@@ -0,0 +1,105 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include "bpf_helpers.h"
+
+#include "common_kern_user.h" /* defines: struct datarec; */
+
+/* Lesson: See how a map is defined.
+ * - Here an array with XDP_ACTION_MAX (max_)entries are created.
+ * - The idea is to keep stats per (enum) xdp_action
+ */
+struct bpf_map_def SEC("maps") xdp_stats_map = {
+	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.key_size    = sizeof(__u32),
+	.value_size  = sizeof(struct datarec),
+	.max_entries = XDP_ACTION_MAX,
+};
+
+/* LLVM maps __sync_fetch_and_add() as a built-in function to the BPF atomic add
+ * instruction (that is BPF_STX | BPF_XADD | BPF_W for word sizes)
+ */
+#ifndef lock_xadd
+#define lock_xadd(ptr, val)	((void) __sync_fetch_and_add(ptr, val))
+#endif
+
+static __always_inline
+__u32 record_xdp_stats_action(struct xdp_md *ctx, __u32 action)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
+
+	if (action >= XDP_ACTION_MAX)
+		return XDP_ABORTED;
+
+	/* Lookup in kernel BPF-side return pointer to actual data record */
+	struct datarec *rec = bpf_map_lookup_elem(&xdp_stats_map, &action);
+	if (!rec)
+		return XDP_ABORTED;
+
+	/* Calculate packet length */
+	__u64 bytes = data_end - data;
+
+	/* BPF_MAP_TYPE_PERCPU_ARRAY returns a data record specific to current
+	 * CPU and XDP hooks runs under Softirq, which makes it safe to update
+	 * without atomic operations.
+	 */
+	rec->rx_packets++;
+	rec->rx_bytes += bytes;
+
+	return action;
+}
+
+SEC("xdp_pass")
+int  xdp_pass_func(struct xdp_md *ctx)
+{
+	__u32 action = XDP_PASS; /* XDP_PASS = 2 */
+
+	return record_xdp_stats_action(ctx, action);
+}
+
+SEC("xdp_drop")
+int  xdp_drop_func(struct xdp_md *ctx)
+{
+	__u32 action = XDP_DROP;
+
+	return record_xdp_stats_action(ctx, action);
+}
+
+SEC("xdp_abort")
+int  xdp_abort_func(struct xdp_md *ctx)
+{
+	__u32 action = XDP_ABORTED;
+
+	return record_xdp_stats_action(ctx, action);
+}
+
+char _license[] SEC("license") = "GPL";
+
+/* Copied from: $KERNEL/include/uapi/linux/bpf.h
+ *
+ * User return codes for XDP prog type.
+ * A valid XDP program must return one of these defined values. All other
+ * return codes are reserved for future use. Unknown return codes will
+ * result in packet drops and a warning via bpf_warn_invalid_xdp_action().
+ *
+enum xdp_action {
+	XDP_ABORTED = 0,
+	XDP_DROP,
+	XDP_PASS,
+	XDP_TX,
+	XDP_REDIRECT,
+};
+
+ * user accessible metadata for XDP packet hook
+ * new fields must be added to the end of this structure
+ *
+struct xdp_md {
+	// (Note: type __u32 is NOT the real-type)
+	__u32 data;
+	__u32 data_end;
+	__u32 data_meta;
+	// Below access go through struct xdp_rxq_info
+	__u32 ingress_ifindex; // rxq->dev->ifindex
+	__u32 rx_queue_index;  // rxq->queue_index
+};
+*/
