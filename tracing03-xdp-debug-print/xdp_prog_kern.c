7b22e4e065a03a212f78cc1143a6db82d5083eaf
Jiri Olsa <jolsa@kernel.org>
2019-05-08 15:34:42 +0200

    Adding tracing03-xdp-debug-print example
    In this lesson we will show how to print message from
    eBPF program into tracefs buffer.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

---
+++ tracing03-xdp-debug-print/xdp_prog_kern.c
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_vlan.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+#include <stdbool.h>
+#include "bpf_helpers.h"
+#include "bpf_endian.h"
+
+#define bpf_printk(fmt, ...)                                    \
+({                                                              \
+	char ____fmt[] = fmt;                                   \
+	bpf_trace_printk(____fmt, sizeof(____fmt),              \
+                         ##__VA_ARGS__);                        \
+})
+
+/* to u64 in host order */
+static inline __u64 ether_addr_to_u64(const __u8 *addr)
+{
+	__u64 u = 0;
+	int i;
+
+	for (i = ETH_ALEN; i >= 0; i--)
+		u = u << 8 | addr[i];
+	return u;
+}
+
+SEC("xdp")
+int  xdp_prog_simple(struct xdp_md *ctx)
+{
+	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	struct ethhdr *eth = data;
+	__u64 offset = sizeof(*eth);
+
+	if ((void *)eth + offset > data_end)
+		return 0;
+
+	bpf_printk("src: %llu, dst: %llu, proto: %u\n",
+		   ether_addr_to_u64(eth->h_source),
+		   ether_addr_to_u64(eth->h_dest),
+		   bpf_ntohs(eth->h_proto));
+
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
