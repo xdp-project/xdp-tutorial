c8d9b90d48f347b78da5c3bdd7535378a3cf3a95
Anton Protopopov <a.s.protopopov@gmail.com>
2019-05-10 20:10:55 +0000

    testenv: fix the --legacy-ip option implementation
    The implementation of the '--legacy-ip' option was broken in two places.
    
    First, the 'ip route' program was called to add a IPv4 route with a network
    prefix ending with the IPv6 '::' notation.
    
    Second, the 'get_num' function returns the number of a test environment in hex,
    and this return value is used when defining IPv6 and/or IPv4 network prefixes.
    In the IPv4 case we need to convert it to decimal, or some prefixes, starting
    from the tenth test environment, e.g., '10.11.a' or '10.11.b', will be incorrect.
    
    Signed-off-by: Anton Protopopov <a.s.protopopov@gmail.com>

--- testenv/testenv.sh
+++ testenv/testenv.sh
@@ -196,7 +196,7 @@ setup()
     local NUM=$(get_num "$NS")
     local PEERNAME="testl-ve-$NUM"
     [ -z "$IP6_PREFIX" ] && IP6_PREFIX="${IP6_SUBNET}:${NUM}::"
-    [ -z "$IP4_PREFIX" ] && IP4_PREFIX="${IP4_SUBNET}.${NUM}."
+    [ -z "$IP4_PREFIX" ] && IP4_PREFIX="${IP4_SUBNET}.$((0x$NUM))."
 
     INSIDE_IP6="${IP6_PREFIX}2"
     INSIDE_IP4="${IP4_PREFIX}2"
@@ -240,7 +240,7 @@ setup()
         ip -n "$NS" addr add dev veth0 "${INSIDE_IP4}/${IP4_PREFIX_SIZE}"
         ip neigh add "$INSIDE_IP4" lladdr "$INSIDE_MAC" dev "$NS" nud permanent
         ip -n "$NS" neigh add "$OUTSIDE_IP4" lladdr "$OUTSIDE_MAC" dev veth0 nud permanent
-        ip -n "$NS" route add "${IP4_SUBNET}::/$IP4_FULL_PREFIX_SIZE" via "$OUTSIDE_IP4" dev veth0
+        ip -n "$NS" route add "${IP4_SUBNET}/${IP4_FULL_PREFIX_SIZE}" via "$OUTSIDE_IP4" dev veth0
         ENABLE_IPV4=1
     else
         ENABLE_IPV4=0
