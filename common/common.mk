xdp-project/xdp-tutorial

commit 0c73a0bb7daee989cd6e7a673dd985d283351e9e

Makefile common, let COPY_STATS add extra deps
If Makefile defines a need to copy over xdp_stats, when
create dependencies to common/xdp_stats_kern*.h.

Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -65,6 +65,8 @@ ifdef COPY_STATS
 $(COPY_STATS): $(LOADER_DIR)/${COPY_STATS:=.c} $(COMMON_H)
 	make -C $(LOADER_DIR) $(COPY_STATS)
 	cp $(LOADER_DIR)/$(COPY_STATS) $(COPY_STATS)
+# Needing xdp_stats imply depending on header files:
+EXTRA_DEPS := $(COMMON_DIR)/xdp_stats_kern.h $(COMMON_DIR)/xdp_stats_kern_user.h
 endif
 
 # For build dependency on this file, if it gets updated

commit 29fcd4a4e165052e4ffbd41fabe44f1c258d363b

Makefile common.mk: Fix that EXTRA_DEPS was overwritten
If defining COPY_STATS then EXTRA_DEPS variable was overwritten.
Thus, the subdir Makefile EXTRA_DEPS defines were lost.
Fix this by using "+=" instead of ":=".

This bug was introduced in commit 0c73a0bb7dae ("Makefile common,
let COPY_STATS add extra deps").

Fixes: 0c73a0bb7dae ("Makefile common, let COPY_STATS add extra deps")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

common/common.mk
@@ -68,7 +68,7 @@ $(COPY_STATS): $(LOADER_DIR)/${COPY_STATS:=.c} $(COMMON_H)
 	make -C $(LOADER_DIR) $(COPY_STATS)
 	cp $(LOADER_DIR)/$(COPY_STATS) $(COPY_STATS)
 # Needing xdp_stats imply depending on header files:
-EXTRA_DEPS := $(COMMON_DIR)/xdp_stats_kern.h $(COMMON_DIR)/xdp_stats_kern_user.h
+EXTRA_DEPS += $(COMMON_DIR)/xdp_stats_kern.h $(COMMON_DIR)/xdp_stats_kern_user.h
 endif
 
 # For build dependency on this file, if it gets updated

packet-solutions/Makefile
@@ -8,6 +8,6 @@ COMMON_DIR = ../common
 
 COPY_LOADER := xdp_loader
 COPY_STATS  := xdp_stats
-EXTRA_DEPS := $(COMMON_DIR)/parsing_helpers.h
+EXTRA_DEPS  += $(COMMON_DIR)/parsing_helpers.h
 
 include $(COMMON_DIR)/common.mk