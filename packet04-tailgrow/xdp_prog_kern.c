f988eec12bc4cd402674acaecc413a5f04a8eb5f
Jesper Dangaard Brouer <brouer@redhat.com>
2020-05-01 17:14:40 +0200

    packet04-tailgrow: cleanup, remove unused code
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- packet04-tailgrow/xdp_prog_kern.c
+++ packet04-tailgrow/xdp_prog_kern.c
@@ -12,84 +12,14 @@
 #include "../common/xdp_stats_kern_user.h"
 #include "../common/xdp_stats_kern.h"
 
-#ifndef memcpy
-#define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
-#endif
-
-struct bpf_map_def SEC("maps") tx_port = {
-	.type = BPF_MAP_TYPE_DEVMAP,
-	.key_size = sizeof(int),
-	.value_size = sizeof(int),
-	.max_entries = 256,
-};
-
-struct bpf_map_def SEC("maps") redirect_params = {
-	.type = BPF_MAP_TYPE_HASH,
-	.key_size = ETH_ALEN,
-	.value_size = ETH_ALEN,
-	.max_entries = 1,
-};
-
-/* Solution to packet03/assignment-2 */
-SEC("xdp_redirect")
-int xdp_redirect_func(struct xdp_md *ctx)
+SEC("xdp_tailgrow")
+int tailgrow_pass(struct xdp_md *ctx)
 {
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct hdr_cursor nh;
-	struct ethhdr *eth;
-	int eth_type;
-	int action = XDP_PASS;
-	unsigned char dst[ETH_ALEN] = { /* TODO: put your values here */ };
-	unsigned ifindex = 0/* TODO: put your values here */;
+	int offset;
 
-	/* These keep track of the next header type and iterator pointer */
-	nh.pos = data;
-
-	/* Parse Ethernet and IP/IPv6 headers */
-	eth_type = parse_ethhdr(&nh, data_end, &eth);
-	if (eth_type == -1)
-		goto out;
-
-	/* Set a proper destination address */
-	memcpy(eth->h_dest, dst, ETH_ALEN);
-	action = bpf_redirect(ifindex, 0);
-
-out:
-	return xdp_stats_record_action(ctx, action);
-}
-
-/* Solution to packet03/assignment-3 */
-SEC("xdp_redirect_map")
-int xdp_redirect_map_func(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct hdr_cursor nh;
-	struct ethhdr *eth;
-	int eth_type;
-	int action = XDP_PASS;
-	unsigned char *dst;
-
-	/* These keep track of the next header type and iterator pointer */
-	nh.pos = data;
-
-	/* Parse Ethernet and IP/IPv6 headers */
-	eth_type = parse_ethhdr(&nh, data_end, &eth);
-	if (eth_type == -1)
-		goto out;
-
-	/* Do we know where to redirect this packet? */
-	dst = bpf_map_lookup_elem(&redirect_params, eth->h_source);
-	if (!dst)
-		goto out;
-
-	/* Set a proper destination address */
-	memcpy(eth->h_dest, dst, ETH_ALEN);
-	action = bpf_redirect_map(&tx_port, 0, 0);
-
-out:
-	return xdp_stats_record_action(ctx, action);
+	offset = 10;
+	bpf_xdp_adjust_tail(ctx, offset);
+	return xdp_stats_record_action(ctx, XDP_PASS);
 }
 
 SEC("xdp_pass")
@@ -98,25 +28,10 @@ int xdp_pass_func(struct xdp_md *ctx)
 	return xdp_stats_record_action(ctx, XDP_PASS);
 }
 
-SEC("xdp_tailgrow")
-int xdp_tailgrow_func(struct xdp_md *ctx)
-{
-//	void *data_end = (void *)(long)ctx->data_end;
-//	void *data = (void *)(long)ctx->data;
-	//struct hdr_cursor nh;
-	int offset;
-
-	offset = 10;
-	bpf_xdp_adjust_tail(ctx, offset);
-	return xdp_stats_record_action(ctx, XDP_PASS);
-}
-
+/* For benchmarking tail grow overhead (does a memset)*/
 SEC("xdp_tailgrow_tx")
-int xdp_tailgrow_func2(struct xdp_md *ctx)
+int tailgrow_tx(struct xdp_md *ctx)
 {
-//	void *data_end = (void *)(long)ctx->data_end;
-//	void *data = (void *)(long)ctx->data;
-	//struct hdr_cursor nh;
 	int offset;
 
 	offset = 32;
@@ -124,8 +39,9 @@ int xdp_tailgrow_func2(struct xdp_md *ctx)
 	return xdp_stats_record_action(ctx, XDP_TX);
 }
 
+/* Baseline benchmark of XDP_TX */
 SEC("xdp_tx")
-int xdp_tx_func(struct xdp_md *ctx)
+int xdp_tx_rec(struct xdp_md *ctx)
 {
 	return xdp_stats_record_action(ctx, XDP_TX);
 }
@@ -134,34 +50,6 @@ struct my_timestamp {
 	__u16 magic;
 	__u64 time;
 } __attribute__((packed));
-
-SEC("xdp_tailgrow_use")
-int xdp_tailgrow3(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	//struct hdr_cursor nh;
-	int offset;
-	struct my_timestamp *ts;
-
-	offset = 8;
-//	if (data + offset > data_end)
-//		return XDP_ABORTED;
-
-	bpf_xdp_adjust_tail(ctx, offset);
-	data_end = (void *)(long)ctx->data_end;
-	data = (void *)(long)ctx->data;
-
-	if (data + offset > data_end)
-		return XDP_ABORTED;
-//	if (data + 2048 > data_end)
-//		return XDP_ABORTED;
-
-	ts = data;
-//	ts->time = 42;
-
-	return xdp_stats_record_action(ctx, XDP_PASS);
-}
 
 SEC("xdp_tailgrow_parse")
 int grow_parse(struct xdp_md *ctx)
