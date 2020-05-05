4a627a53f53e30c1de66d6f7de4bc84e9cd3e416
Jesper Dangaard Brouer <brouer@redhat.com>
2020-05-05 19:04:00 +0200

    experiment01-tailgrow: Clean up example xdp_prog_kern2.c
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- experiment01-tailgrow/xdp_prog_kern2.c
+++ experiment01-tailgrow/xdp_prog_kern2.c
@@ -1,22 +1,22 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/bpf.h>
-#include <linux/in.h>
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
-
-// The parsing helper functions from the packet01 lesson have moved here
-#include "../common/parsing_helpers.h"
-#include "../common/rewrite_helpers.h"
-
-/* Defines xdp_stats_map */
-#include "../common/xdp_stats_kern_user.h"
-#include "../common/xdp_stats_kern.h"
 
 #define MTU 1536
-#define MIN_LEN 64
+#define MIN_LEN 14
 
-SEC("xdp_test1b")
-int _xdp_test1b(struct xdp_md *ctx)
+/*
+ * This example show howto access packet last byte in XDP packet,
+ * without parsing packet contents.
+ *
+ * It is not very effecient, as it advance the data pointer one-byte in a
+ * loop until reaching data_end.  This is needed as the verifier only allows
+ * accessing data via advancing the position of the data pointer. The bounded
+ * loop with a max number of iterations allows the verifier to see the bound.
+ */
+
+SEC("xdp_end_loop")
+int _xdp_end_loop(struct xdp_md *ctx)
 {
 	void *data_end = (void *)(long)ctx->data_end;
 	void *data     = (void *)(long)ctx->data;
@@ -24,11 +24,12 @@ int _xdp_test1b(struct xdp_md *ctx)
 	unsigned int i;
 	void *pos;
 
+	/* Assume minimum length to reduce loops needed a bit */
 	unsigned int offset = MIN_LEN;
 
 	pos = data;
 
-	/* Verifier can handle this bounded basic-loop construct */
+	/* Verifier can handle this bounded 'basic-loop' construct */
 	for (i = 0; i < (MTU - MIN_LEN); i++ ) {
 		if (pos + offset > data_end) {
 			/* Promise verifier no access beyond data_end */
@@ -54,80 +55,9 @@ int _xdp_test1b(struct xdp_md *ctx)
 	goto out;
 
 read:
-//	ptr = pos + (offset - );
-//	ptr = pos + (offset - sizeof(*ptr) - 1);
-	ptr = pos + (offset - sizeof(*ptr));
+	ptr = pos + (offset - sizeof(*ptr)); /* Parentheses needed */
 	if (*ptr == 0xFF)
 		return XDP_ABORTED;
 out:
 	return XDP_PASS;
 }
-
-
-SEC("xdp_test1")
-int _xdp_test1(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data     = (void *)(long)ctx->data;
-	unsigned char *ptr;
-	unsigned int i;
-	void *pos;
-
-	unsigned int offset = 64;
-
-	pos = data;
-
-	if (pos + 64 > data_end)
-		goto out;
-
-	for (i = 0; i < (1536-64); i++ ) {
-		if (pos + offset > data_end) {
-			goto out;
-		}
-		if (pos + offset == data_end) {
-			goto read;
-		}
-		offset++;
-	}
-	goto out;
-
-read:
-//	ptr = pos + (offset - );
-//	ptr = pos + (offset - sizeof(*ptr) - 1);
-	ptr = pos + (offset - sizeof(*ptr));
-	if (*ptr == 0xFF)
-		return XDP_ABORTED;
-out:
-	return XDP_PASS;
-}
-
-
-SEC("xdp_test2")
-int _xdp_test2(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct hdr_cursor nh;
-	unsigned char *ptr;
-
-	unsigned int offset = 64;
-
-	nh.pos = data;
-
-	if (nh.pos + offset > data_end)
-		goto out;
-
-//	ptr = nh.pos + (offset - 1);
-	ptr = nh.pos + (offset - sizeof(*ptr));
-	if (*ptr == 0xFF)
-		return XDP_ABORTED;
-out:
-	return xdp_stats_record_action(ctx, XDP_PASS);
-}
-
-SEC("xdp_pass")
-int xdp_pass_f1(struct xdp_md *ctx)
-{
-	return xdp_stats_record_action(ctx, XDP_PASS);
-}
-
