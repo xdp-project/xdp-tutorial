db3f2d50a90c24b8e871574d6fc1709ede032ea3
Chris Ward <tjcw@uk.ibm.com>
2022-07-25 09:24:05 +0000

    basic02 assignment

--- basic02-prog-by-name/xdp_prog_kern.c
+++ basic02-prog-by-name/xdp_prog_kern.c
@@ -26,6 +26,12 @@ int  xdp_drop_func(struct xdp_md *ctx)
 	return XDP_DROP;
 }
 
+SEC("xdp_abort")
+int  xdp_abort_func(struct xdp_md *ctx)
+{
+	return XDP_ABORTED;
+}
+
 /* Assignment#2: Add new XDP program section that use XDP_ABORTED */
 
 char _license[] SEC("license") = "GPL";
