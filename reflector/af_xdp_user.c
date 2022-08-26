102fa4755c87f0879ef2dff51b8eafa690194d43
Chris Ward <tjcw@uk.ibm.com>
2022-08-26 16:57:24 +0100

    waypoint

--- reflector/af_xdp_user.c
+++ reflector/af_xdp_user.c
@@ -272,10 +272,10 @@ static inline void csum_replace2(__sum16 *sum, __be16 old, __be16 new)
 	*sum = ~csum16_add(csum16_sub(~(*sum), old), new);
 }
 
-static bool process_packet(struct xsk_socket_info *xsk,
+static bool process_packet(struct xsk_socket_info *xsk_dst, struct xsk_socket_info xsk_src,
 			   uint64_t addr, uint32_t len)
 {
-	uint8_t *pkt = xsk_umem__get_data(xsk->umem->buffer, addr);
+	uint8_t *pkt = xsk_umem__get_data(xsk_src->umem->buffer, addr);
 
         /* Lesson#3: Write an IPv6 ICMP ECHO parser to send responses
 	 *
@@ -289,45 +289,55 @@ static bool process_packet(struct xsk_socket_info *xsk,
 	if (true) {
 		int ret;
 		uint32_t tx_idx = 0;
-		uint8_t tmp_mac[ETH_ALEN];
-		struct in6_addr tmp_ip;
-		struct ethhdr *eth = (struct ethhdr *) pkt;
-		struct ipv6hdr *ipv6 = (struct ipv6hdr *) (eth + 1);
-		struct icmp6hdr *icmp = (struct icmp6hdr *) (ipv6 + 1);
+		uint64_t tx_frame;
+//		uint8_t tmp_mac[ETH_ALEN];
+//		struct in6_addr tmp_ip;
+//		struct ethhdr *eth = (struct ethhdr *) pkt;
+//		struct ipv6hdr *ipv6 = (struct ipv6hdr *) (eth + 1);
+//		struct icmp6hdr *icmp = (struct icmp6hdr *) (ipv6 + 1);
+//
+//		if (ntohs(eth->h_proto) != ETH_P_IPV6 ||
+//		    len < (sizeof(*eth) + sizeof(*ipv6) + sizeof(*icmp)) ||
+//		    ipv6->nexthdr != IPPROTO_ICMPV6 ||
+//		    icmp->icmp6_type != ICMPV6_ECHO_REQUEST)
+//			return false;
+//
+//		memcpy(tmp_mac, eth->h_dest, ETH_ALEN);
+//		memcpy(eth->h_dest, eth->h_source, ETH_ALEN);
+//		memcpy(eth->h_source, tmp_mac, ETH_ALEN);
+//
+//		memcpy(&tmp_ip, &ipv6->saddr, sizeof(tmp_ip));
+//		memcpy(&ipv6->saddr, &ipv6->daddr, sizeof(tmp_ip));
+//		memcpy(&ipv6->daddr, &tmp_ip, sizeof(tmp_ip));
+//
+//		icmp->icmp6_type = ICMPV6_ECHO_REPLY;
+//
+//		csum_replace2(&icmp->icmp6_cksum,
+//			      htons(ICMPV6_ECHO_REQUEST << 8),
+//			      htons(ICMPV6_ECHO_REPLY << 8));
+//
+//		/* Here we sent the packet out of the receive port. Note that
+//		 * we allocate one entry and schedule it. Your design would be
+//		 * faster if you do batch processing/transmission */
 
-		if (ntohs(eth->h_proto) != ETH_P_IPV6 ||
-		    len < (sizeof(*eth) + sizeof(*ipv6) + sizeof(*icmp)) ||
-		    ipv6->nexthdr != IPPROTO_ICMPV6 ||
-		    icmp->icmp6_type != ICMPV6_ECHO_REQUEST)
-			return false;
-
-		memcpy(tmp_mac, eth->h_dest, ETH_ALEN);
-		memcpy(eth->h_dest, eth->h_source, ETH_ALEN);
-		memcpy(eth->h_source, tmp_mac, ETH_ALEN);
-
-		memcpy(&tmp_ip, &ipv6->saddr, sizeof(tmp_ip));
-		memcpy(&ipv6->saddr, &ipv6->daddr, sizeof(tmp_ip));
-		memcpy(&ipv6->daddr, &tmp_ip, sizeof(tmp_ip));
-
-		icmp->icmp6_type = ICMPV6_ECHO_REPLY;
-
-		csum_replace2(&icmp->icmp6_cksum,
-			      htons(ICMPV6_ECHO_REQUEST << 8),
-			      htons(ICMPV6_ECHO_REPLY << 8));
-
-		/* Here we sent the packet out of the receive port. Note that
-		 * we allocate one entry and schedule it. Your design would be
-		 * faster if you do batch processing/transmission */
-
-		ret = xsk_ring_prod__reserve(&xsk->tx, 1, &tx_idx);
+//		tx_frame = xsk_alloc_umem_frame(xsk_dst) ;
+//		if ( tx_frame == INVALID_UMEM_FRAME ) {
+//			/* No more transmit frames, drop the packet */
+//			return false ;
+//		}
+		ret = xsk_ring_prod__reserve(&xsk_dst->tx, 1, &tx_idx);
 		if (ret != 1) {
 			/* No more transmit slots, drop the packet */
 			return false;
 		}
+		struct xdp_desc tx_desc=xsk_ring_prod__tx_desc(&xsk_dst->tx, tx_idx);
+		memcpy(tx_desc.addr, pkt, len) ;
+		tx_desc.len = len ;
+		xsk_ring_prod__submit(&xsk_dst->tx, 1) ;
 
-		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->addr = addr;
-		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->len = len;
-		xsk_ring_prod__submit(&xsk->tx, 1);
+//		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->addr = addr;
+//		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->len = len;
+//		xsk_ring_prod__submit(&xsk->tx, 1);
 		xsk->outstanding_tx++;
 
 		xsk->stats.tx_bytes += len;
@@ -338,72 +348,76 @@ static bool process_packet(struct xsk_socket_info *xsk,
 	return false;
 }
 
-static void handle_receive_packets(struct xsk_socket_info *xsk)
+static void handle_receive_packets(struct xsk_socket_info *xsk_dst, struct xsk_socket_info *xsk_src)
 {
 	unsigned int rcvd, stock_frames, i;
 	uint32_t idx_rx = 0, idx_fq = 0;
 	int ret;
 
-	rcvd = xsk_ring_cons__peek(&xsk->rx, RX_BATCH_SIZE, &idx_rx);
+	rcvd = xsk_ring_cons__peek(&xsk_src->rx, RX_BATCH_SIZE, &idx_rx);
 	if (!rcvd)
 		return;
 
 	/* Stuff the ring with as much frames as possible */
-	stock_frames = xsk_prod_nb_free(&xsk->umem->fq,
-					xsk_umem_free_frames(xsk));
+	stock_frames = xsk_prod_nb_free(&xsk_src->umem->fq,
+					xsk_umem_free_frames(xsk_src));
 
 	if (stock_frames > 0) {
 
-		ret = xsk_ring_prod__reserve(&xsk->umem->fq, stock_frames,
+		ret = xsk_ring_prod__reserve(&xsk_src->umem->fq, stock_frames,
 					     &idx_fq);
 
 		/* This should not happen, but just in case */
 		while (ret != stock_frames)
-			ret = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd,
+			ret = xsk_ring_prod__reserve(&xsk_src->umem->fq, rcvd,
 						     &idx_fq);
 
 		for (i = 0; i < stock_frames; i++)
-			*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) =
-				xsk_alloc_umem_frame(xsk);
+			*xsk_ring_prod__fill_addr(&xsk_src->umem->fq, idx_fq++) =
+				xsk_alloc_umem_frame(xsk_src);
 
-		xsk_ring_prod__submit(&xsk->umem->fq, stock_frames);
+		xsk_ring_prod__submit(&xsk_src->umem->fq, stock_frames);
 	}
 
 	/* Process received packets */
 	for (i = 0; i < rcvd; i++) {
-		uint64_t addr = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx)->addr;
-		uint32_t len = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx++)->len;
+		uint64_t addr = xsk_ring_cons__rx_desc(&xsk_src->rx, idx_rx)->addr;
+		uint32_t len = xsk_ring_cons__rx_desc(&xsk_src->rx, idx_rx++)->len;
 
-		if (!process_packet(xsk, addr, len))
-			xsk_free_umem_frame(xsk, addr);
+		if (!process_packet(xsk_dst, xsk_src, addr, len))
+			xsk_free_umem_frame(xsk_src, addr);
 
-		xsk->stats.rx_bytes += len;
+		xsk_src->stats.rx_bytes += len;
 	}
 
-	xsk_ring_cons__release(&xsk->rx, rcvd);
-	xsk->stats.rx_packets += rcvd;
+	xsk_ring_cons__release(&xsk_src->rx, rcvd);
+	xsk_src->stats.rx_packets += rcvd;
 
 	/* Do we need to wake up the kernel for transmission */
-	complete_tx(xsk);
+	complete_tx(xsk_dst);
   }
 
 static void rx_and_process(struct config *cfg,
-			   struct xsk_socket_info *xsk_socket)
+			   struct xsk_socket_info *xsk_socket_0, struct xsk_socket_info *xsk_socket_1)
 {
 	struct pollfd fds[2];
-	int ret, nfds = 1;
+	int ret, nfds = 2;
 
 	memset(fds, 0, sizeof(fds));
-	fds[0].fd = xsk_socket__fd(xsk_socket->xsk);
+	fds[0].fd = xsk_src_socket__fd(xsk_socket_0->xsk);
 	fds[0].events = POLLIN;
+	fds[1].fd = xsk_src_socket__fd(xsk_socket_1->xsk);
+	fds[1].events = POLLIN;
 
 	while(!global_exit) {
-		if (cfg->xsk_poll_mode) {
+//		if (cfg->xsk_poll_mode) {
 			ret = poll(fds, nfds, -1);
-			if (ret <= 0 || ret > 1)
+			if (ret <= 0 || ret > 2)
 				continue;
-		}
-		handle_receive_packets(xsk_socket);
+//		}
+		if ( fds[0].revents & POLLIN ) handle_received_packets(xsk_socket_1, xsk_socket_0) ;
+		if ( fds[1].revents & POLLIN ) handle_received_packets(xsk_socket_0, xsk_socket_1) ;
+//		handle_receive_packets(xsk_socket);
 	}
 }
 
