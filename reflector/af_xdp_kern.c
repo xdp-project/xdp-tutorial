2bee8d9a975c7299b9771232c3b740d5200c6659
Chris Ward <tjcw@uk.ibm.com>
2022-08-26 15:34:20 +0100

    BPF to send all packets to user space

--- reflector/af_xdp_kern.c
+++ reflector/af_xdp_kern.c
@@ -25,26 +25,26 @@ struct {
 	__type(value, int);
 } xsks_map SEC(".maps") ;
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__uint(max_entries, 64);
-	__type(key, int);
-	__type(value, __u32);
-} xdp_stats_map SEC(".maps");
+//struct {
+//	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+//	__uint(max_entries, 64);
+//	__type(key, int);
+//	__type(value, __u32);
+//} xdp_stats_map SEC(".maps");
 
 SEC("xdp_sock")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
     int index = ctx->rx_queue_index;
-    __u32 *pkt_count;
+//    __u32 *pkt_count;
 
-    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-    if (pkt_count) {
-
-        /* We pass every other packet */
-        if ((*pkt_count)++ & 1)
-            return XDP_PASS;
-    }
+//    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
+//    if (pkt_count) {
+//
+//        /* We pass every other packet */
+//        if ((*pkt_count)++ & 1)
+//            return XDP_PASS;
+//    }
 
     /* A set entry here means that the correspnding queue_id
      * has an active AF_XDP socket bound to it. */
