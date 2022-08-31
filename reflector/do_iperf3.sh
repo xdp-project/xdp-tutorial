983c8fcc9905273dfbbcefa0ca016fb6db7b103f
Chris Ward <tjcw@uk.ibm.com>
2022-08-31 15:39:35 +0100

    Back to normal

--- reflector/do_iperf3.sh
+++ reflector/do_iperf3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-ip netns exec ns2 strace iperf3 -s &
+ip netns exec ns2 iperf3 -s &
 pid=$!
-sleep 2
-ip netns exec ns1 iperf3 -c 10.10.0.20 -u -t 60
+sleep 1
+ip netns exec ns1 iperf3 -c 10.10.0.20 -t 60
 kill $pid
 wait
