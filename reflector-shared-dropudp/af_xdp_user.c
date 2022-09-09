d7793cc7f8c1cc3c475c509448531f8459fc856a
Chris Ward <tjcw@uk.ibm.com>
2022-09-09 13:14:35 +0100

    Turn off instrumentation

--- reflector-shared-dropudp/af_xdp_user.c
+++ reflector-shared-dropudp/af_xdp_user.c
@@ -31,8 +31,8 @@
 #include "../common/common_user_bpf_xdp.h"
 #include "../common/common_libbpf.h"
 
-#define INSTRUMENT 1
-#define VERIFY_UMEM 1
+#define INSTRUMENT 0
+#define VERIFY_UMEM 0
 
 #define NUM_FRAMES         4096
 #define FRAME_SIZE         XSK_UMEM__DEFAULT_FRAME_SIZE
