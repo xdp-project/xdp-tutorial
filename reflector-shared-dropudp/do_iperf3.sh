ab06b8dfe57cef7350dcac880785d6beac55c4cb
Chris Ward <tjcw@uk.ibm.com>
2022-09-09 14:10:59 +0100

    Set repeating payload

--- reflector-shared-dropudp/do_iperf3.sh
+++ reflector-shared-dropudp/do_iperf3.sh
@@ -2,6 +2,6 @@
 ip netns exec ns2 iperf3 -s &
 pid=$!
 sleep 1
-ip netns exec ns1 iperf3 -c 10.10.0.20 -u -t 60 -b 0
+ip netns exec ns1 iperf3 -c 10.10.0.20 -u -t 60 -b 0 --repeating-payload
 kill $pid
 wait
