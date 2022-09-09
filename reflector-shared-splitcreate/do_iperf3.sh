12bbe46d1a2022f70e30ad00bc0c75859a165713
Chris Ward <tjcw@uk.ibm.com>
2022-09-09 15:27:59 +0100

    Turn off Nagle

--- reflector-shared-splitcreate/do_iperf3.sh
+++ reflector-shared-splitcreate/do_iperf3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-ip netns exec ns2 iperf3 -s &
+ip netns exec ns2 iperf3 -s -N &
 pid=$!
 sleep 1
-ip netns exec ns1 iperf3 -c 10.10.0.20 -t 60
+ip netns exec ns1 iperf3 -c 10.10.0.20 -t 60 -N
 kill $pid
 wait
