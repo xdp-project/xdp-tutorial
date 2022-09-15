594d5a2a2c0867a99c4b80bbca9cf5d94b893215
Chris Ward <tjcw@uk.ibm.com>
2022-09-15 11:14:14 +0100

    Fix route command

--- udp-sender/run_veth_client.sh
+++ udp-sender/run_veth_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-route add -net 10.10.0.10/16 gw 10.1.0.2
+route add -net 10.10.0.0/16 gw 10.1.0.2
 ./udp-sender 10.10.0.10 8000
-route del -net 10.10.0.10/16
+route del -net 10.10.0.0/16
 
