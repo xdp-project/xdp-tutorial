15434449a2bcc7596cfc3e3d1ec34583f75620be
Chris Ward <tjcw@uk.ibm.com>
2022-09-21 14:36:38 +0100

    Fix test case

--- udp-sender/run2local.sh
+++ udp-sender/run2local.sh
@@ -17,20 +17,16 @@ ip netns exec ns2 ip link set lo up
 ip netns exec ns1 ip link set vpeer1 up
 ip netns exec ns2 ip link set vpeer2 up
 
-#ip netns exec ns1 ip addr add 10.10.0.10/16 dev vpeer1
 ip netns exec ns1 ip addr add 10.1.0.253/24 dev vpeer1
-ip netns exec ns1 ip addr add 10.1.0.252/24 dev vpeer2
+ip netns exec ns2 ip addr add 10.1.0.252/24 dev vpeer2
 
 ip link add br0 type bridge
 ip link set br0 up
 
 ip link set veth1 master br0
 ip link set veth2 master br0
-#ip link set ens1 master br0
 
 ip addr add 10.1.0.254/24 dev br0
-
-#ip netns exec ns1 ip route add default via 10.10.0.1
 
 iptables -P FORWARD ACCEPT
 iptables -F FORWARD
