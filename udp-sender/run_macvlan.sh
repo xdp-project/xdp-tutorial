5d693b66f4a5e0a1b7dabed31f32d9dec4f3c15f
Chris Ward <tjcw@uk.ibm.com>
2022-09-21 15:11:54 +0100

    try macvlan

---
+++ udp-sender/run_macvlan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+ip netns add ns1
+
+ip netns exec ns1 ip link set lo up
+
+ip link add macvlan1 link ens1 type macvlan mode bridge
+ip netns exec ns1 ip link set macvlan1 up
+ip netns exec ns1 ip addr add 10.1.0.253/24 dev macvlan1
+
+iptables -P FORWARD ACCEPT
+iptables -F FORWARD
+
+export LD_LIBRARY_PATH=/usr/local/lib
+ip netns exec ns1 ./af_xdp_user -d macvlan1
+#gdb ./af_xdp_user
+
