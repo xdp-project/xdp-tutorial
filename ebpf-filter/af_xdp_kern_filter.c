f38e97d0b999c7b2ea4200eef84eb3882070b1cb
Chris Ward <tjcw@uk.ibm.com>
2022-10-06 11:16:21 +0100

    unroll the loop

--- ebpf-filter/af_xdp_kern_filter.c
+++ ebpf-filter/af_xdp_kern_filter.c
@@ -107,6 +107,7 @@ int xdp_sock_prog(struct xdp_md *ctx)
 
     int index = ctx->rx_queue_index;
     int a;
+#pragma unroll
     for(a=0; a<64; a+=1)
     {
     	void * mapped0=bpf_map_lookup_elem(&xsks_map, &a) ;
