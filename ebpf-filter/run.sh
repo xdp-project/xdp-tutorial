fcb07e7271d4696af2606cb5ab4255df1a0287d0
Chris Ward <tjcw@uk.ibm.com>
2022-10-11 13:37:16 +0100

    Wrong eBPF program

--- ebpf-filter/run.sh
+++ ebpf-filter/run.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 ip tuntap add mode tap tap0
 ip link set dev tap0 up
-./af_xdp_user -S -d enp25s0 -Q 0 --filename ./af_xdp_kern_filter.o
+./af_xdp_user -S -d enp25s0 -Q 0 --filename ./af_xdp_kern_pass.o
