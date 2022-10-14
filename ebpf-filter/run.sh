a7e1a6bea30ff8733ba6cfdee62a04440bc499be
Chris Ward <tjcw@uk.ibm.com>
2022-10-14 16:20:25 +0100

    waypoint

--- ebpf-filter/run.sh
+++ ebpf-filter/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 export LD_LIBRARY_PATH=/usr/local/lib
 ip tuntap add mode tun tun0
-ip link set addr 10.2.0.1/16
+ip link set dev tun0 addr 10.1.1.2/24
 ip link set dev tun0 up
 ./af_xdp_user -S -d enp25s0 -Q 0 --filename ./af_xdp_kern_pass.o
