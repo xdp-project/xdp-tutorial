f226c1ce798661a548dfbba16561c2c498d1beda
Chris Ward <tjcw@uk.ibm.com>
2022-10-14 15:42:35 +0100

    Set address for tun/tap

--- ebpf-filter/run.sh
+++ ebpf-filter/run.sh
@@ -2,4 +2,5 @@
 export LD_LIBRARY_PATH=/usr/local/lib
 ip tuntap add mode tun tun0
 ip link set dev tun0 up
+ip link set addr 10.1.0.254
 ./af_xdp_user -S -d enp25s0 -Q 0 --filename ./af_xdp_kern_pass.o
