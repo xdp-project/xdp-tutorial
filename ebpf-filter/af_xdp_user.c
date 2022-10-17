a77dc0b8302897978231cba533d4b49c272459b8
Chris Ward <tjcw@uk.ibm.com>
2022-10-17 15:40:32 +0100

    rearrance open_bpf_map_file and checker

--- ebpf-filter/af_xdp_user.c
+++ ebpf-filter/af_xdp_user.c
@@ -783,70 +783,70 @@ int tun_alloc(char *dev)
       return fd;
   }
 
-//static int open_bpf_map_file(const char *pin_dir,
-//		      const char *mapname,
-//		      struct bpf_map_info *info)
-//{
-//	char filename[PATH_MAX];
-//	int err, len, fd;
-//	__u32 info_len = sizeof(*info);
-//
-//	len = snprintf(filename, PATH_MAX, "%s/%s", pin_dir, mapname);
-//	if (len < 0) {
-//		fprintf(stderr, "ERR: constructing full mapname path\n");
-//		return -1;
-//	}
-//
-//	fd = bpf_obj_get(filename);
-//	if (fd < 0) {
-//		fprintf(stderr,
-//			"WARN: Failed to open bpf map file:%s err(%d):%s\n",
-//			filename, errno, strerror(errno));
-//		return fd;
-//	}
-//
-//	if (info) {
-//		err = bpf_obj_get_info_by_fd(fd, info, &info_len);
-//		if (err) {
-//			fprintf(stderr, "ERR: %s() can't get info - %s\n",
-//				__func__,  strerror(errno));
-//			return EXIT_FAIL_BPF;
-//		}
-//	}
-//
-//	return fd;
-//}
+static int open_bpf_map_file(const char *pin_dir,
+		      const char *mapname,
+		      struct bpf_map_info *info)
+{
+	char filename[PATH_MAX];
+	int err, len, fd;
+	__u32 info_len = sizeof(*info);
 
-//static int check_map_fd_info(const struct bpf_map_info *info,
-//		      const struct bpf_map_info *exp)
-//{
-//	if (exp->key_size && exp->key_size != info->key_size) {
-//		fprintf(stderr, "ERR: %s() "
-//			"Map key size(%d) mismatch expected size(%d)\n",
-//			__func__, info->key_size, exp->key_size);
-//		return EXIT_FAIL;
-//	}
-//	if (exp->value_size && exp->value_size != info->value_size) {
-//		fprintf(stderr, "ERR: %s() "
-//			"Map value size(%d) mismatch expected size(%d)\n",
-//			__func__, info->value_size, exp->value_size);
-//		return EXIT_FAIL;
-//	}
-//	if (exp->max_entries && exp->max_entries != info->max_entries) {
-//		fprintf(stderr, "ERR: %s() "
-//			"Map max_entries(%d) mismatch expected size(%d)\n",
-//			__func__, info->max_entries, exp->max_entries);
-//		return EXIT_FAIL;
-//	}
-//	if (exp->type && exp->type  != info->type) {
-//		fprintf(stderr, "ERR: %s() "
-//			"Map type(%d) mismatch expected type(%d)\n",
-//			__func__, info->type, exp->type);
-//		return EXIT_FAIL;
-//	}
-//
-//	return 0;
-//}
+	len = snprintf(filename, PATH_MAX, "%s/%s", pin_dir, mapname);
+	if (len < 0) {
+		fprintf(stderr, "ERR: constructing full mapname path\n");
+		return -1;
+	}
+
+	fd = bpf_obj_get(filename);
+	if (fd < 0) {
+		fprintf(stderr,
+			"WARN: Failed to open bpf map file:%s err(%d):%s\n",
+			filename, errno, strerror(errno));
+		return fd;
+	}
+
+	if (info) {
+		err = bpf_obj_get_info_by_fd(fd, info, &info_len);
+		if (err) {
+			fprintf(stderr, "ERR: %s() can't get info - %s\n",
+				__func__,  strerror(errno));
+			return EXIT_FAIL_BPF;
+		}
+	}
+
+	return fd;
+}
+
+static int check_map_fd_info(const struct bpf_map_info *info,
+		      const struct bpf_map_info *exp)
+{
+	if (exp->key_size && exp->key_size != info->key_size) {
+		fprintf(stderr, "ERR: %s() "
+			"Map key size(%d) mismatch expected size(%d)\n",
+			__func__, info->key_size, exp->key_size);
+		return EXIT_FAIL;
+	}
+	if (exp->value_size && exp->value_size != info->value_size) {
+		fprintf(stderr, "ERR: %s() "
+			"Map value size(%d) mismatch expected size(%d)\n",
+			__func__, info->value_size, exp->value_size);
+		return EXIT_FAIL;
+	}
+	if (exp->max_entries && exp->max_entries != info->max_entries) {
+		fprintf(stderr, "ERR: %s() "
+			"Map max_entries(%d) mismatch expected size(%d)\n",
+			__func__, info->max_entries, exp->max_entries);
+		return EXIT_FAIL;
+	}
+	if (exp->type && exp->type  != info->type) {
+		fprintf(stderr, "ERR: %s() "
+			"Map type(%d) mismatch expected type(%d)\n",
+			__func__, info->type, exp->type);
+		return EXIT_FAIL;
+	}
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
