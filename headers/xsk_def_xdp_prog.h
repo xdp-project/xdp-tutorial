7bba57d39f583dfb864b763e5a3ec05fe18d7563
Chris Ward <tjcw@uk.ibm.com>
2022-10-20 11:30:42 +0100

    place xsk_def_xdp_prog.h where it will be picked up

---
+++ headers/xsk_def_xdp_prog.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
+
+#ifndef __LIBXDP_XSK_DEF_XDP_PROG_H
+#define __LIBXDP_XSK_DEF_XDP_PROG_H
+
+#define XDP_METADATA_SECTION "xdp_metadata"
+#define XSK_PROG_VERSION 1
+
+#endif /* __LIBXDP_XSK_DEF_XDP_PROG_H */
