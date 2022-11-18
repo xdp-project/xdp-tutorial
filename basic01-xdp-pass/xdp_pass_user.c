2528830308e87a9240cf0edd46df315ba6f71a76
Donald Hunter <donald.hunter@gmail.com>
2022-11-18 12:06:08 +0000

    Rewrite basic01 to use libxdp
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

--- basic01-xdp-pass/xdp_pass_user.c
+++ basic01-xdp-pass/xdp_pass_user.c
@@ -7,8 +7,10 @@ static const char *__doc__ = "Simple XDP prog doing XDP_PASS\n";
 #include <errno.h>
 #include <getopt.h>
 
+#include <linux/bpf.h>
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
+#include <xdp/libxdp.h>
 
 #include <net/if.h>
 #include <linux/if_link.h> /* depend on kernel-headers installed */
@@ -40,95 +42,16 @@ static const struct option_wrapper long_options[] = {
 	{{0, 0, NULL,  0 }, NULL, false}
 };
 
-int load_bpf_object_file__simple(const char *filename)
-{
-	int first_prog_fd = -1;
-	struct bpf_object *obj;
-	int err;
-
-	/* Use libbpf for extracting BPF byte-code from BPF-ELF object, and
-	 * loading this into the kernel via bpf-syscall
-	 */
-	err = bpf_prog_load(filename, BPF_PROG_TYPE_XDP, &obj, &first_prog_fd);
-	if (err) {
-		fprintf(stderr, "ERR: loading BPF-OBJ file(%s) (%d): %s\n",
-			filename, err, strerror(-err));
-		return -1;
-	}
-
-	/* Simply return the first program file descriptor.
-	 * (Hint: This will get more advanced later)
-	 */
-	return first_prog_fd;
-}
-
-static int xdp_link_detach(int ifindex, __u32 xdp_flags)
-{
-	/* Next assignment this will move into ../common/
-	 * (in more generic version)
-	 */
-	int err;
-
-	if ((err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags)) < 0) {
-		fprintf(stderr, "ERR: link set xdp unload failed (err=%d):%s\n",
-			err, strerror(-err));
-		return EXIT_FAIL_XDP;
-	}
-	return EXIT_OK;
-}
-
-int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd)
-{
-	/* Next assignment this will move into ../common/ */
-	int err;
-
-	/* libbpf provide the XDP net_device link-level hook attach helper */
-	err = bpf_set_link_xdp_fd(ifindex, prog_fd, xdp_flags);
-	if (err == -EEXIST && !(xdp_flags & XDP_FLAGS_UPDATE_IF_NOEXIST)) {
-		/* Force mode didn't work, probably because a program of the
-		 * opposite type is loaded. Let's unload that and try loading
-		 * again.
-		 */
-
-		__u32 old_flags = xdp_flags;
-
-		xdp_flags &= ~XDP_FLAGS_MODES;
-		xdp_flags |= (old_flags & XDP_FLAGS_SKB_MODE) ? XDP_FLAGS_DRV_MODE : XDP_FLAGS_SKB_MODE;
-		err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags);
-		if (!err)
-			err = bpf_set_link_xdp_fd(ifindex, prog_fd, old_flags);
-	}
-
-	if (err < 0) {
-		fprintf(stderr, "ERR: "
-			"ifindex(%d) link set xdp fd failed (%d): %s\n",
-			ifindex, -err, strerror(-err));
-
-		switch (-err) {
-		case EBUSY:
-		case EEXIST:
-			fprintf(stderr, "Hint: XDP already loaded on device"
-				" use --force to swap/replace\n");
-			break;
-		case EOPNOTSUPP:
-			fprintf(stderr, "Hint: Native-XDP not supported"
-				" use --skb-mode or --auto-mode\n");
-			break;
-		default:
-			break;
-		}
-		return EXIT_FAIL_XDP;
-	}
-
-	return EXIT_OK;
-}
 
 int main(int argc, char **argv)
 {
-	struct bpf_prog_info info = {};
-	__u32 info_len = sizeof(info);
-	char filename[256] = "xdp_pass_kern.o";
-	int prog_fd, err;
+	DECLARE_LIBBPF_OPTS(bpf_object_open_opts, opts);
+	DECLARE_LIBXDP_OPTS(xdp_program_opts, xdp_opts, 0);
+	char filename[] = "xdp_pass_kern.o";
+	char progname[] = "xdp_pass_simple";
+	char errmsg[1024];
+	int err = EXIT_SUCCESS;
+	struct xdp_program *p;
 
 	struct config cfg = {
 		.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
@@ -143,24 +66,33 @@ int main(int argc, char **argv)
 		usage(argv[0], __doc__, long_options, (argc == 1));
 		return EXIT_FAIL_OPTION;
 	}
-	if (cfg.do_unload)
-		return xdp_link_detach(cfg.ifindex, cfg.xdp_flags);
+	/* if (cfg.do_unload) */
+	/* 	return xdp_link_detach(cfg.ifindex, cfg.xdp_flags); */
 
-	/* Load the BPF-ELF object file and get back first BPF_prog FD */
-	prog_fd = load_bpf_object_file__simple(filename);
-	if (prog_fd <= 0) {
-		fprintf(stderr, "ERR: loading file: %s\n", filename);
-		return EXIT_FAIL_BPF;
+	xdp_opts.open_filename = filename;
+	xdp_opts.prog_name = progname;
+	xdp_opts.opts = &opts;
+
+	p = xdp_program__create(&xdp_opts);
+	err = libxdp_get_error(p);
+	if (err) {
+		libxdp_strerror(err, errmsg, sizeof(errmsg));
+		fprintf(stderr, "Couldn't create XDP program %s: %s\n",
+			progname, errmsg);
+		return err;
 	}
 
-	/* At this point: BPF-prog is (only) loaded by the kernel, and prog_fd
-	 * is our file-descriptor handle. Next step is attaching this FD to a
-	 * kernel hook point, in this case XDP net_device link-level hook.
-	 * Fortunately libbpf have a helper for this:
-	 */
-	err = xdp_link_attach(cfg.ifindex, cfg.xdp_flags, prog_fd);
-	if (err)
+	err = xdp_program__attach(p, cfg.ifindex, XDP_MODE_SKB, 0);
+	if (err) {
+		libxdp_strerror(err, errmsg, sizeof(errmsg));
+		fprintf(stderr, "Couldn't attach XDP program on iface '%s' : %s (%d)\n",
+			cfg.ifname, errmsg, err);
 		return err;
+	}
+
+	struct bpf_prog_info info = {};
+	__u32 info_len = sizeof(info);
+	int prog_fd = xdp_program__fd(p);
 
         /* This step is not really needed , BPF-info via bpf-syscall */
 	err = bpf_obj_get_info_by_fd(prog_fd, &info, &info_len);
