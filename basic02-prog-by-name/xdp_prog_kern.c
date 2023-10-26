4b68405277c20d3eae22a6918df6e60cc73a59b0
Baoxd123 <xb2029@nyu.edu>
2023-10-26 05:37:22 +0000

    basic02-xdp_prog_kern.c test

--- basic02-prog-by-name/xdp_prog_kern.c
+++ basic02-prog-by-name/xdp_prog_kern.c
@@ -27,6 +27,11 @@ int  xdp_drop_func(struct xdp_md *ctx)
 }
 
 /* Assignment#2: Add new XDP program section that use XDP_ABORTED */
+SEC("xdp_abort")
+int  xdp_abort_func(struct xdp_md *ctx)
+{
+    return XDP_ABORTED;
+}
 
 char _license[] SEC("license") = "GPL";
 
